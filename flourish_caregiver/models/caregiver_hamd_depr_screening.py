bdb9603516b7712e197b459aa686a74e14780823
ThatoKgamaetsile <thatokgamaetsile@gmail.com>
2020-11-04 11:38:20 +0200

    Adding depression score calculation for HAM-D  caregiver

--- flourish_caregiver/models/caregiver_hamd_depr_screening.py
+++ flourish_caregiver/models/caregiver_hamd_depr_screening.py
@@ -17,17 +17,17 @@ class CaregiverHamdDeprScreening(CrfModelMixin):
         max_length=2)
 
     guilt_feelings = models.CharField(
-        verbose_name=('Feelings Of Guilt'),
+        verbose_name='Feelings Of Guilt',
         choices=GUILT_FEELINGS,
         max_length=2)
 
     suicidal = models.CharField(
-        verbose_name=('Suicide'),
+        verbose_name='Suicide',
         choices=SUICIDAL,
         max_length=2)
 
     insomnia_initial = models.CharField(
-        verbose_name=('Insomnia – Initial (Difficulty in falling asleep)'),
+        verbose_name='Insomnia – Initial (Difficulty in falling asleep)',
         choices=INSOMNIA_INITIAL,
         max_length=2)
 
@@ -44,7 +44,7 @@ class CaregiverHamdDeprScreening(CrfModelMixin):
         max_length=2)
 
     work_interests = models.CharField(
-        verbose_name=('Work and Interests'),
+        verbose_name='Work and Interests',
         choices=WORK_INTERESTS,
         max_length=2)
 
@@ -55,12 +55,12 @@ class CaregiverHamdDeprScreening(CrfModelMixin):
         max_length=2)
 
     agitation = models.CharField(
-        verbose_name=('Agitation (Restlessness associated with anxiety.)'),
+        verbose_name='Agitation (Restlessness associated with anxiety.)',
         choices=AGITATION,
         max_length=2)
 
     anxiety_pyschic = models.CharField(
-        verbose_name=('Anxiety – Psychic'),
+        verbose_name='Anxiety – Psychic',
         choices=ANXIETY_PYSCHIC,
         max_length=2)
 
@@ -79,30 +79,56 @@ class CaregiverHamdDeprScreening(CrfModelMixin):
 
     general_symptoms = models.CharField(
         verbose_name=('Somatic Symptoms – General (Heaviness in limbs, back or'
-                      ' head; diffuse backache; loss of energy and fatiguability)'),
+                      ' head; diffuse backache; loss of energy '
+                      'and fatiguability)'),
         choices=GENERAL_SOMATIC,
         max_length=2)
 
     genital_symptoms = models.CharField(
-        verbose_name=('Genital Symptoms (Loss of libido, menstrual disturbances)'),
+        verbose_name='Genital Symptoms (Loss of libido, menstrual '
+                     'disturbances)',
         choices=GENITAL_SYMPTOMS,
         max_length=2)
 
     hypochondriasis = models.CharField(
-        verbose_name=('Hypochondriasis'),
+        verbose_name='Hypochondriasis',
         choices=HYPOCHONDRIASIS,
         max_length=2)
 
     weight_loss = models.CharField(
-        verbose_name=('Weight Loss'),
+        verbose_name='Weight Loss',
         choices=WEIGHT_LOSS,
         max_length=2)
 
     insight = models.CharField(
-        verbose_name=('Insight'),
+        verbose_name='Insight',
         choices=INSIGHT,
         max_length=2,
-        help_text='(Insight must be interpreted in terms of patient’s understanding and background.)')
+        help_text='(Insight must be interpreted in terms of patient’s '
+                  'understanding and background.)')
+
+    depression_score = models.IntegerField(
+        verbose_name='Depression score',
+        default="",
+        null=True,
+        blank=True)
+
+    def save(self, *args, **kwargs):
+        self.depression_score = self.calculate_depression_score()
+        super().save(*args, **kwargs)
+
+    def calculate_depression_score(self):
+        score = 0
+        for f in self._meta.get_fields():
+            if f.name in ['depressed_mood', 'guilt_feelings', 'suicidal',
+                          'insomnia_initial', 'insomnia_middle',
+                          'insomnia_delayed', 'work_interests', 'retardation',
+                          'agitation', 'anxiety_pyschic', 'anxiety',
+                          'gastro_symptoms', 'general_symptoms',
+                          'genital_symptoms', 'hypochondriasis', 'weight_loss',
+                          'insight', ]:
+                score += int(getattr(self, f.name))
+        return score
 
     class Meta(CrfModelMixin.Meta):
         app_label = 'flourish_caregiver'
