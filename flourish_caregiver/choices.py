e631a858fc4d035cdff1d99156b46113b4550b79
ThatoKgamaetsile <thatokgamaetsile@gmail.com>
2020-11-05 22:41:51 +0200

    Arranging choices alphabetically

--- flourish_caregiver/choices.py
+++ flourish_caregiver/choices.py
@@ -8,6 +8,13 @@ from edc_visit_tracking.constants import SCHEDULED, UNSCHEDULED, LOST_VISIT
 from .constants import BREASTFEED_ONLY, NEVER_STARTED, MODIFIED, TUBERCULOSIS
 from .constants import NO_MODIFICATIONS, START
 
+ABLE_TO_LAUGH = (
+    ('0', 'As much as I always could'),
+    ('1', 'Not quite so much now'),
+    ('2', 'Definitely not so much now'),
+    ('3', 'Not at all'),
+)
+
 AGITATION = (
     ('0', 'None'),
     ('1', 'Fidgetiness'),
@@ -16,51 +23,38 @@ AGITATION = (
     ('4', 'Hand wringing, nail biting, hair-pulling, biting of lips.')
 )
 
-LOCATOR_LOG_STATUS = (
-    ('exist', 'Exists'),
-    ('not_found', 'Not found')
-    )
-
-IDENTITY_TYPE = (
-    ('country_id', 'Country ID number'),
-    ('country_id_rcpt', 'Country ID receipt'),
-    ('passport', 'Passport'),
-    (OTHER, 'Other'),
-)
-
 ALIVE_DEAD_UNKNOWN = (
     (ALIVE, 'Alive'),
     (DEAD, 'Dead'),
     (UNKNOWN, 'Unknown'),
 )
 
-VISIT_INFO_SOURCE = [
-    (PARTICIPANT, 'Clinic visit with participant'),
-    ('other_contact',
-     'Other contact with participant (for example telephone call)'),
-    ('other_doctor',
-     'Contact with external health care provider/medical doctor'),
-    ('family',
-     'Contact with family or designated person who can provide information'),
-    ('chart', 'Hospital chart or other medical record'),
-    (OTHER, 'Other')]
+AMNIOTIC_FLUID = (
+    ('0', 'Normal'),
+    ('1', 'Abnormal')
+)
 
-VISIT_REASON = [
-    (SCHEDULED, 'Scheduled visit/contact'),
-    (MISSED_VISIT, 'Missed Scheduled visit'),
-    (UNSCHEDULED,
-     'Unscheduled visit at which lab samples or data are being submitted'),
-    (LOST_VISIT, 'Lost to follow-up (use only when taking subject off study)'),
-    (FAILED_ELIGIBILITY, 'Subject failed enrollment eligibility'),
-    (COMPLETED_PROTOCOL_VISIT, 'Subject has completed the study')]
+ANXIETY_PYSCHIC = (
+    ('0', 'No difficulty'),
+    ('1', 'Subjective tension and irritability'),
+    ('2', 'Worrying about minor matters'),
+    ('3', 'Apprehensive attitude apparent in face or speech'),
+    ('4', 'Fears expressed without questioning')
+)
 
-ARV_INTERRUPTION_REASON = (
-    ('TOXICITY', 'Toxicity'),
-    ('NO_DRUGS', 'No drugs available'),
-    ('NO_REFILL', 'Didn\'t get to clinic for refill'),
-    ('FORGOT', 'Mother forgot to take the ARVs'),
-    (OTHER, 'Other'),
-    (NOT_APPLICABLE, 'Not Applicable'),
+ANXIETY = (
+    ('0', 'Absent'),
+    ('1', 'Mild'),
+    ('2', 'Moderate'),
+    ('3', 'Severe'),
+    ('4', 'Incapacitating')
+)
+
+ANXIOUS = (
+    ('0', 'No, not at all'),
+    ('1', 'Hardly ever'),
+    ('2', 'Yes, sometimes'),
+    ('3', 'Yes, very often')
 )
 
 ARV_DRUG_LIST = (
@@ -83,13 +77,13 @@ ARV_DRUG_LIST = (
     ('HAART,unknown', 'HAART,unknown'),
 )
 
-FEEDING_CHOICES = (
-    (BREASTFEED_ONLY, 'Breastfeed only'),
-    ('Formula feeding only', 'Formula feeding only'),
-    ('Both breastfeeding and formula feeding',
-     'Both breastfeeding and formula feeding'),
-    ('Medical complications: Infant did not feed',
-     'Medical complications: Infant did not feed'),
+ARV_INTERRUPTION_REASON = (
+    ('TOXICITY', 'Toxicity'),
+    ('NO_DRUGS', 'No drugs available'),
+    ('NO_REFILL', 'Didn\'t get to clinic for refill'),
+    ('FORGOT', 'Mother forgot to take the ARVs'),
+    (OTHER, 'Other'),
+    (NOT_APPLICABLE, 'Not Applicable'),
 )
 
 CONSENT_VERSION = (
@@ -104,6 +98,110 @@ COWS_MILK = (
     (NOT_APPLICABLE, 'Not Applicable'),
 )
 
+CRYING = (
+    ('3', 'Yes, most of the time'),
+    ('2, quite often', 'Yes, quite often'),
+    ('1', 'Only occasionally'),
+    ('0', 'No, never')
+)
+
+DEPRESSION_SCALE = (
+    ('0', 'Not at all'),
+    ('1', 'Several days'),
+    ('2', 'More than half the days'),
+    ('3', 'Nearly every day'),
+)
+
+DEPRESSION_MOOD = (
+    ('0', 'Absent'),
+    ('1', 'These feeling states indicated only on questioning'),
+    ('2', 'These feeling states spontaneously reported verbally'),
+    ('3', ('Communicates feeling states non-verbally, i.e. through facial '
+           'expression, posture, voice and tendency to weep')),
+    ('4', ('Patient reports virtually only these feeling states in his/her '
+           'spontaneous verbal and non-verbal communication.'))
+)
+
+ENJOYMENT_TO_THINGS = (
+    ('0', 'As much as I ever did'),
+    ('1', 'Rather less than I used to'),
+    ('2', 'Definitely less than I used to'),
+    ('3', 'Hardly at all'),
+)
+
+FEEDING_CHOICES = (
+    (BREASTFEED_ONLY, 'Breastfeed only'),
+    ('Formula feeding only', 'Formula feeding only'),
+    ('Both breastfeeding and formula feeding',
+     'Both breastfeeding and formula feeding'),
+    ('Medical complications: Infant did not feed',
+     'Medical complications: Infant did not feed'),
+)
+
+FOOD_FREQUENCY = (
+    ('0', 'Often True'),
+    ('1', 'Sometimes True'),
+    ('2', 'Never True'),
+    ("3", "I don’t know or Refused to answer"),
+)
+
+GENERAL_SOMATIC = (
+    ('0', 'None'),
+    ('1', ('Heaviness in limbs, back or head. Backaches, headaches, '
+           'muscle aches. Loss of energy and fatigability.')),
+    ('2', ('Any clear-cut symptom rates 2.')),
+)
+
+GENITAL_SYMPTOMS = (
+    ('0', 'Absent'),
+    ('1', 'Mild'),
+    ('2', 'Severe')
+)
+
+GESTATIONS_NUMBER = (
+    ('0', '0'),
+    ('1', '1'),
+    ('2', '2'),
+    ('3', '3')
+)
+
+GUILT_FEELINGS = (
+    ('0', 'Absent'),
+    ('1', 'Self-reproach, feels he/she has let people down'),
+    ('2', 'Ideas of guilt or rumination over past errors or sinful deeds.'),
+    ('3', 'Present illness is a punishment; delusions of guilt'),
+    ('4', 'Hears accusatory or denunciatory voices and/or experiences threatening visual hallucinations.')
+)
+
+HARM = (
+    ('3', 'Yes, quite often'),
+    ('2', 'Sometimes'),
+    ('1', 'Hardly ever'),
+    ('0', 'Never')
+)
+
+HOW_OFTEN = (
+    ('0', 'Acknowledges being depressed and ill'),
+    ('1', 'Acknowledges illness but attributes cause to bad food, climate, '
+          'overwork, virus, need for rest, etc'),
+    ('2', 'Denies being ill at all'),
+)
+
+HYPOCHONDRIASIS = (
+    ('0', 'Not present'),
+    ('1', 'Self-absorption (bodily)'),
+    ('2', 'Preoccupation with health'),
+    ('3', 'Frequent complaints, requests for help, etc.'),
+    ('4', 'Hypochondriacal delusions')
+)
+
+IDENTITY_TYPE = (
+    ('country_id', 'Country ID number'),
+    ('country_id_rcpt', 'Country ID receipt'),
+    ('passport', 'Passport'),
+    (OTHER, 'Other'),
+)
+
 INFO_PROVIDER = (
     ('MOTHER', 'Mother'),
     ('GRANDMOTHER', 'Grandmother'),
@@ -113,11 +211,68 @@ INFO_PROVIDER = (
     (OTHER, 'Other'),
 )
 
+INSIGHT = (
+    ('0', 'Acknowledges being depressed and ill'),
+    ('1', ('Acknowledges illness but attributes cause to bad food, climate, '
+           'overwork, virus, need for rest, etc')),
+    ('2', 'Denies being ill at all')
+)
+
+INSOMNIA_INITIAL = (
+    ('0', 'No difficulty falling asleep.'),
+    ('1', 'Complains of occasional difficulty falling asleep, i.e. more than 1⁄2 hour.'),
+    ('2', 'Complains of nightly difficulty falling asleep')
+)
+
+INSOMIA_MIDNIGHT = (
+    ('0', 'No difficulty.'),
+    ('1', 'Patient complains of being restless and disturbed during the night.'),
+    ('2', 'Waking during the night – any getting out of bed rates 2 (except for purposes of voiding).')
+)
+
+INSOMNIA_EARLY = (
+    ('0', 'No difficulty.'),
+    ('1', 'Waking in early hours of the morning but goes back to sleep.'),
+    ('2', 'Unable to fall asleep again if he/she gets out of bed.'),
+)
+
+LOCATOR_LOG_STATUS = (
+    ('exist', 'Exists'),
+    ('not_found', 'Not found')
+    )
+
 MATERNAL_VISIT_STUDY_STATUS = (
     (ON_STUDY, 'On study'),
     (OFF_STUDY,
      'Off study-no further follow-up (including death); use only '
      'for last study contact'),
+)
+
+MEALS = (
+    ('0', 'Yes'),
+    ('1', 'No'),
+    ("2", "I don’t know"),
+)
+
+MISERABLE_FEELING = (
+    ('0', 'No, not at all'),
+    ('1', 'Not very often'),
+    ('2', 'Yes, quite often'),
+    ('3', 'Yes, most of the time'),
+)
+
+PANICK = (
+    ('3', 'Yes, quite a lot'),
+    ('2, sometimes', 'Yes, sometimes'),
+    ('1', 'No, not much'),
+    ('0', 'No, not at all')
+)
+
+REASON_ARV_STOP = (
+    ('switch for tolerability', 'Switch for tolerability'),
+    ('switch for drug outage', 'Switch for drug outage'),
+    ('Treatment failure', 'Treatment failure'),
+    (OTHER, 'Other, specify:')
 )
 
 REASONS_VACCINES_MISSED = (
@@ -130,6 +285,64 @@ REASONS_VACCINES_MISSED = (
     (OTHER, 'Other, specify'),
 )
 
+REFERRED_TO = (
+    ('Community Social Worker', 'Community Social Worker'),
+    ('Hospitalbased Social Worker', 'Hospitalbased Social Worker'),
+    ('A&E', 'A&E'),
+    ('Psychologist', 'Psychologist'),
+    ('Psychiatrist', 'Psychiatrist'),
+    (OTHER, 'Other'),
+)
+
+RELATION_TO_CHILD = (
+    ('father', 'Father'),
+    ('grandmother', 'Grandmother'),
+    ('grandfather', 'Grandfather'),
+    ('aunt', 'Aunt'),
+    ('uncle', 'Uncle'),
+    ('sister', 'Sister'),
+    ('brother', 'Brother'),
+    ('guardian', 'Guardian'),
+    (OTHER, 'Other'),
+)
+
+RETARDATION = (
+    ('0', 'Normal speech and thought.'),
+    ('1', 'Slight retardation during the interview.'),
+    ('2', 'Obvious retardation during the interview.'),
+    ('3', 'Interview difficult'),
+    ('4', 'Complete stupor')
+)
+
+SLEEPING_DIFFICULTY = (
+    ('0', 'No, not at all'),
+    ('1', 'Not very often'),
+    ('2', 'Yes, sometimes'),
+    ('3', 'Yes, most of the time'),
+)
+
+SELF_BLAME = (
+    ('0', 'No, never'),
+    ('1', 'Not very often'),
+    ('2', 'Yes, some of the time'),
+    ('3', 'Yes, most of the time'),
+)
+
+SOMATIC_SYMPTOMS = (
+    ('0', 'None'),
+    ('1', 'Loss of appetite but eating without staff encouragement. Heavy feelings in abdomen.'),
+    ('2', ('Difficulty eating without staff urging. Requests or requires '
+           'laxatives or medication for bowels or medication for gastro-intestinal symptoms.')),
+)
+
+SUICIDAL = (
+    ('0', 'Absent'),
+    ('1', 'Feels life is not worth living'),
+    ('2', 'Wishes he/she were dead or any thoughts of possible death to self.'),
+    ('3', 'Suicidal ideas or gestures'),
+    ('4', 'Attempts at suicide')
+)
+
 TIMES_BREASTFED = (
     ('<1 per week', '1. Less than once per week'),
     ('<1 per day, but at least once per week',
@@ -140,6 +353,13 @@ TIMES_BREASTFED = (
     ('For all feedings',
      '5. For all feedings (i.e no formula or other foods or liquids)'),
     (NOT_APPLICABLE, 'Not Applicable'),
+)
+
+TOP = (
+    ('3', 'Yes, most of the time I haven\'t been able to cope at all'),
+    ('2', 'Yes, sometimes I haven\'t been coping as well as usual'),
+    ('1', 'No, most of the time I have coped quite well'),
+    ('0', 'No, I have been coping as well as ever')
 )
 
 TRISOME_CHROSOMESOME_ABNORMALITY = (
@@ -163,128 +383,6 @@ WATER_USED = (
     (NOT_APPLICABLE, 'Not Applicable'),
 )
 
-GESTATIONS_NUMBER = (
-    ('0', '0'),
-    ('1', '1'),
-    ('2', '2'),
-    ('3', '3')
-)
-
-ZERO_ONE = (
-    ('0', '0'),
-    ('1', '1')
-)
-
-ANXIOUS = (
-    ('0', 'No, not at all'),
-    ('1', 'Hardly ever'),
-    ('2', 'Yes, sometimes'),
-    ('3', 'Yes, very often')
-)
-
-PANICK = (
-    ('3', 'Yes, quite a lot'),
-    ('2, sometimes', 'Yes, sometimes'),
-    ('1', 'No, not much'),
-    ('0', 'No, not at all')
-)
-
-TOP = (
-    ('3', 'Yes, most of the time I haven\'t been able to cope at all'),
-    ('2', 'Yes, sometimes I haven\'t been coping as well as usual'),
-    ('1', 'No, most of the time I have coped quite well'),
-    ('0', 'No, I have been coping as well as ever')
-)
-
-CRYING = (
-    ('3', 'Yes, most of the time'),
-    ('2, quite often', 'Yes, quite often'),
-    ('1', 'Only occasionally'),
-    ('0', 'No, never')
-)
-
-HARM = (
-    ('3', 'Yes, quite often'),
-    ('2', 'Sometimes'),
-    ('1', 'Hardly ever'),
-    ('0', 'Never')
-)
-
-AMNIOTIC_FLUID = (
-    ('0', 'Normal'),
-    ('1', 'Abnormal')
-)
-
-REASON_ARV_STOP = (
-    ('switch for tolerability', 'Switch for tolerability'),
-    ('switch for drug outage', 'Switch for drug outage'),
-    ('Treatment failure', 'Treatment failure'),
-    (OTHER, 'Other, specify:')
-)
-
-RELATION_TO_CHILD = (
-    ('father', 'Father'),
-    ('grandmother', 'Grandmother'),
-    ('grandfather', 'Grandfather'),
-    ('aunt', 'Aunt'),
-    ('uncle', 'Uncle'),
-    ('sister', 'Sister'),
-    ('brother', 'Brother'),
-    ('guardian', 'Guardian'),
-    (OTHER, 'Other'),
-)
-
-DEPRESSION_SCALE = (
-    ('0', 'Not at all'),
-    ('1', 'Several days'),
-    ('2', 'More than half the days'),
-    ('3', 'Nearly every day'),
-)
-
-DEPRESSION_MOOD = (
-    ('0', 'Absent'),
-    ('1', 'These feeling states indicated only on questioning'),
-    ('2', 'These feeling states spontaneously reported verbally'),
-    ('3', ('Communicates feeling states non-verbally, i.e. through facial '
-           'expression, posture, voice and tendency to weep')),
-    ('4', ('Patient reports virtually only these feeling states in his/her '
-           'spontaneous verbal and non-verbal communication.'))
-)
-
-GUILT_FEELINGS = (
-    ('0', 'Absent'),
-    ('1', 'Self-reproach, feels he/she has let people down'),
-    ('2', 'Ideas of guilt or rumination over past errors or sinful deeds.'),
-    ('3', 'Present illness is a punishment; delusions of guilt'),
-    ('4', 'Hears accusatory or denunciatory voices and/or experiences threatening visual hallucinations.')
-)
-
-SUICIDAL = (
-    ('0', 'Absent'),
-    ('1', 'Feels life is not worth living'),
-    ('2', 'Wishes he/she were dead or any thoughts of possible death to self.'),
-    ('3', 'Suicidal ideas or gestures'),
-    ('4', 'Attempts at suicide')
-)
-
-INSOMNIA_INITIAL = (
-    ('0', 'No difficulty falling asleep.'),
-    ('1', 'Complains of occasional difficulty falling asleep, i.e. more than 1⁄2 hour.'),
-    ('2', 'Complains of nightly difficulty falling asleep')
-)
-
-INSOMIA_MIDNIGHT = (
-    ('0', 'No difficulty.'),
-    ('1', 'Patient complains of being restless and disturbed during the night.'),
-    ('2', 'Waking during the night – any getting out of bed rates 2 (except for purposes of voiding).')
-)
-
-INSOMNIA_EARLY = (
-    ('0', 'No difficulty.'),
-    ('1', 'Waking in early hours of the morning but goes back to sleep.'),
-    ('2', 'Unable to fall asleep again if he/she gets out of bed.'),
-)
-
 WORK_INTERESTS = (
     ('0', 'No difficulty'),
     ('1', ('Thoughts and feelings of incapacity, fatigue or weakness related to'
@@ -295,58 +393,6 @@ WORK_INTERESTS = (
     ('4', ('Stopped working because of present illness.'))
 )
 
-RETARDATION = (
-    ('0', 'Normal speech and thought.'),
-    ('1', 'Slight retardation during the interview.'),
-    ('2', 'Obvious retardation during the interview.'),
-    ('3', 'Interview difficult'),
-    ('4', 'Complete stupor')
-)
-
-ANXIETY_PYSCHIC = (
-    ('0', 'No difficulty'),
-    ('1', 'Subjective tension and irritability'),
-    ('2', 'Worrying about minor matters'),
-    ('3', 'Apprehensive attitude apparent in face or speech'),
-    ('4', 'Fears expressed without questioning')
-)
-
-ANXIETY = (
-    ('0', 'Absent'),
-    ('1', 'Mild'),
-    ('2', 'Moderate'),
-    ('3', 'Severe'),
-    ('4', 'Incapacitating')
-)
-
-SOMATIC_SYMPTOMS = (
-    ('0', 'None'),
-    ('1', 'Loss of appetite but eating without staff encouragement. Heavy feelings in abdomen.'),
-    ('2', ('Difficulty eating without staff urging. Requests or requires '
-           'laxatives or medication for bowels or medication for gastro-intestinal symptoms.')),
-)
-
-GENERAL_SOMATIC = (
-    ('0', 'None'),
-    ('1', ('Heaviness in limbs, back or head. Backaches, headaches, '
-           'muscle aches. Loss of energy and fatigability.')),
-    ('2', ('Any clear-cut symptom rates 2.')),
-)
-
-GENITAL_SYMPTOMS = (
-    ('0', 'Absent'),
-    ('1', 'Mild'),
-    ('2', 'Severe')
-)
-
-HYPOCHONDRIASIS = (
-    ('0', 'Not present'),
-    ('1', 'Self-absorption (bodily)'),
-    ('2', 'Preoccupation with health'),
-    ('3', 'Frequent complaints, requests for help, etc.'),
-    ('4', 'Hypochondriacal delusions')
-)
-
 WEIGHT_LOSS = (
     ('0', 'No weight loss'),
     ('1', 'Probable weight loss associated with present illness.'),
@@ -354,73 +400,28 @@ WEIGHT_LOSS = (
     ('3', 'Not assessed.')
 )
 
-INSIGHT = (
-    ('0', 'Acknowledges being depressed and ill'),
-    ('1', ('Acknowledges illness but attributes cause to bad food, climate, '
-           'overwork, virus, need for rest, etc')),
-    ('2', 'Denies being ill at all')
-)
+VISIT_INFO_SOURCE = [
+    (PARTICIPANT, 'Clinic visit with participant'),
+    ('other_contact',
+     'Other contact with participant (for example telephone call)'),
+    ('other_doctor',
+     'Contact with external health care provider/medical doctor'),
+    ('family',
+     'Contact with family or designated person who can provide information'),
+    ('chart', 'Hospital chart or other medical record'),
+    (OTHER, 'Other')]
 
-FOOD_FREQUENCY = (
-    ('0', 'Often True'),
-    ('1', 'Sometimes True'),
-    ('2', 'Never True'),
-    ("3", "I don’t know or Refused to answer"),
-)
+VISIT_REASON = [
+    (SCHEDULED, 'Scheduled visit/contact'),
+    (MISSED_VISIT, 'Missed Scheduled visit'),
+    (UNSCHEDULED,
+     'Unscheduled visit at which lab samples or data are being submitted'),
+    (LOST_VISIT, 'Lost to follow-up (use only when taking subject off study)'),
+    (FAILED_ELIGIBILITY, 'Subject failed enrollment eligibility'),
+    (COMPLETED_PROTOCOL_VISIT, 'Subject has completed the study')]
 
-MEALS = (
-    ('0', 'Yes'),
-    ('1', 'No'),
-    ("2", "I don’t know"),
-)
 
-HOW_OFTEN = (
-    ('0', 'Acknowledges being depressed and ill'),
-    ('1', 'Acknowledges illness but attributes cause to bad food, climate, '
-          'overwork, virus, need for rest, etc'),
-    ('2', 'Denies being ill at all'),
-)
-
-REFERRED_TO = (
-    ('Community Social Worker', 'Community Social Worker'),
-    ('Hospitalbased Social Worker', 'Hospitalbased Social Worker'),
-    ('A&E', 'A&E'),
-    ('Psychologist', 'Psychologist'),
-    ('Psychiatrist', 'Psychiatrist'),
-    (OTHER, 'Other'),
-)
-
-ABLE_TO_LAUGH = (
-    ('0', 'As much as I always could'),
-    ('1', 'Not quite so much now'),
-    ('2', 'Definitely not so much now'),
-    ('3', 'Not at all'),
-)
-
-ENJOYMENT_TO_THINGS = (
-    ('0', 'As much as I ever did'),
-    ('1', 'Rather less than I used to'),
-    ('2', 'Definitely less than I used to'),
-    ('3', 'Hardly at all'),
-)
-
-SELF_BLAME = (
-    ('0', 'No, never'),
-    ('1', 'Not very often'),
-    ('2', 'Yes, some of the time'),
-    ('3', 'Yes, most of the time'),
-)
-
-SLEEPING_DIFFICULTY = (
-    ('0', 'No, not at all'),
-    ('1', 'Not very often'),
-    ('2', 'Yes, sometimes'),
-    ('3', 'Yes, most of the time'),
-)
-
-MISERABLE_FEELING = (
-    ('0', 'No, not at all'),
-    ('1', 'Not very often'),
-    ('2', 'Yes, quite often'),
-    ('3', 'Yes, most of the time'),
+ZERO_ONE = (
+    ('0', '0'),
+    ('1', '1')
 )
