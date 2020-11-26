bab469c07163d8b04058cff879bbe436b1bc237b
ThatoKgamaetsile <thatokgamaetsile@gmail.com>
2020-11-26 12:40:27 +0200

    adding validations to subject consent form

--- flourish_caregiver/forms/subject_consent_form.py
+++ flourish_caregiver/forms/subject_consent_form.py
@@ -2,11 +2,14 @@ from django import forms
 from edc_base.sites import SiteModelFormMixin
 from edc_form_validators import FormValidatorMixin
 
+from flourish_form_validations.form_validators import SubjectConsentFormValidator
 from ..models import SubjectConsent
 
 
 class SubjectConsentForm(SiteModelFormMixin, FormValidatorMixin,
                          forms.ModelForm):
+
+    form_validator_cls = SubjectConsentFormValidator
 
     screening_identifier = forms.CharField(
         label='Screening Identifier',
