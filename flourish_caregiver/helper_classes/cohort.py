104adad2fc3043113e53dff9eb2f400c2a5399b4
COulson Kgathi <ckgathi@gmail.com>
2020-12-08 06:33:36 +0200

    cohort deciding class

--- flourish_caregiver/helper_classes/cohort.py
+++ flourish_caregiver/helper_classes/cohort.py
@@ -9,19 +9,17 @@ class Cohort:
     """Class that determines the cohort that a child belong too
     """
 
-    def __init__(self, delivery_date=None):
-        self.delivery_date = delivery_date
-
+    def __init__(self, child_dob=None, ):
+        self.child_dob = child_dob
 
     def age(self):
         pass
-        # delivdt
-        
+
     def cohort_a(self):
         pass
-    
+
     def cohort_b(self):
         pass
-    
+
     def cohort_c(self):
         pass
---
+++ flourish_caregiver/tests/test_cohort.py
@@ -0,0 +1,10 @@
+from django.test.testcases import TestCase
+
+
+class TestCohort(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_me(self):
+        pass
