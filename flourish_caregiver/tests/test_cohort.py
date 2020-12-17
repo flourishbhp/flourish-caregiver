5109c255bec450cd4844465b29b6c92a98b3d0a6
Coulson Kgathi <ckgathi@gmail.com>
2020-12-17 15:58:03 +0200

    cohort deciding class

--- flourish_caregiver/tests/test_cohort.py
+++ flourish_caregiver/tests/test_cohort.py
@@ -1,10 +1,20 @@
+from dateutil.relativedelta import relativedelta
+from datetime import date
+
 from django.test.testcases import TestCase
+from  django.utils import timezone
 
 
 class TestCohort(TestCase):
 
     def setUp(self):
-        pass
+        self.data = {
+            'enrollment_date': timezone.now().date(),
+            'child_dob': date.today() - relativedelta(years=2),
+            'infant_hiv_exposed': 'Exposed',
+            'mum_hivstatus': None,
+            'protocol': 'Tshilo Dikotla'
+        }
 
-    def test_me(self):
-        pass
+    def test_calculate_age(self):
+        """Test if """
