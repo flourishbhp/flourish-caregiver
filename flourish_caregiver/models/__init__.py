4b887d624a1b78ddf0686ee8aba8b933d6651a51
tumiemosweu <imosweuk@gmail.com>
2021-01-06 09:40:22 +0200

    update oncs onschedule signal

--- flourish_caregiver/models/__init__.py
+++ flourish_caregiver/models/__init__.py
@@ -28,5 +28,6 @@ from .ultrasound import UltraSound
 from .maternal_visit import MaternalVisit
 from .signals import antenatal_enrollment_on_post_save
 from .signals import maternal_dataset_on_post_save
+from .signals import subject_consent_on_post_save
 from .onschedule import OnScheduleCohortA, OnScheduleCohortB
 from .onschedule import OnScheduleCohortC
