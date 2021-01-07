5ffda144c2e66450441b88b17d5ea515807487fd
ame diphoko <diphokoame@gmail.com>
2021-01-07 11:09:15 +0200

    update caregivers prev enrolled form

--- flourish_caregiver/models/caregiver_previously_enrolled.py
+++ flourish_caregiver/models/caregiver_previously_enrolled.py
@@ -4,7 +4,7 @@ from edc_base.model_mixins import BaseUuidModel
 from edc_base.model_validators import datetime_not_future
 from edc_base.sites.site_model_mixin import SiteModelMixin
 from edc_base.utils import get_utcnow
-from edc_constants.choices import YES_NO
+from edc_constants.choices import YES_NO, GENDER
 
 from ..choices import RELATION_TO_CHILD
 from ..maternal_choices import POS_NEG_IND
@@ -32,35 +32,42 @@ class CaregiverPreviouslyEnrolled(SiteModelMixin, BaseUuidModel):
     current_hiv_status = models.CharField(
         verbose_name='What is your current HIV status?',
         choices=POS_NEG_IND,
-        max_length=14)
+        max_length=14,
+        null=True,
+        blank=True,)
 
-    last_test_date = models.DateField(
+    last_test_date = models.CharField(
         verbose_name='Do you know your last HIV test date?',
         choices=YES_NO,
-        max_length=3, )
+        max_length=3,
+        null=True,
+        blank=True,)
 
     test_date = models.DateField(
-        verbose_name='Test Date', )
+        verbose_name='Test Date',
+        null=True,
+        blank=True,)
 
     is_date_estimated = models.CharField(
         verbose_name='Is this date estimated?',
         choices=YES_NO,
-        max_length=3, )
-
-    dob = models.DateField(
-        verbose_name="Date of birth",
+        max_length=3,
         null=True,
-        blank=True,
-        validators=[datetime_not_future, ],)
+        blank=True,)
 
     sex = models.CharField(
         verbose_name='Gender',
-        max_length=7)
+        max_length=7,
+        choices=GENDER,
+        null=True,
+        blank=True,)
 
     relation_to_child = models.CharField(
         verbose_name='Relationship to child',
         choices=RELATION_TO_CHILD,
-        max_length=20, )
+        max_length=20,
+        null=True,
+        blank=True,)
 
     relation_to_child_other = OtherCharField()
 
