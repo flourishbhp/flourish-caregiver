5f0eeb3a50df8196c352de9e8fc779bbd7fad2e0
ThatoKgamaetsile <thatokgamaetsile@gmail.com>
2021-02-22 21:36:51 +0200

    Add Maternal interim idcc form validator cls

--- flourish_caregiver/forms/maternal_interim_idcc_form.py
+++ flourish_caregiver/forms/maternal_interim_idcc_form.py
@@ -2,10 +2,12 @@ from django import forms
 from ..models import MaternalInterimIdcc
 from .form_mixins import SubjectModelFormMixin
 
+from flourish_form_validations.form_validators import MaternalIterimIdccFormValidator
+
 
 class MaternalInterimIdccForm(SubjectModelFormMixin, forms.ModelForm):
 
-    form_validator_cls = None
+    form_validator_cls = MaternalIterimIdccFormValidator
 
     class Meta:
         model = MaternalInterimIdcc
