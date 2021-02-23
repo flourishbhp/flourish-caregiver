185a317249b4a0cddf19b8b56e3aeecd09214986
ame diphoko <diphokoame@gmail.com>
2021-02-23 11:02:38 +0200

    add obsterical history form validator cls

--- flourish_caregiver/forms/obsterical_history_form.py
+++ flourish_caregiver/forms/obsterical_history_form.py
@@ -1,11 +1,13 @@
 from django import forms
+from flourish_form_validations.form_validators import ObstericalHistoryFormValidator
+
 from ..models import ObstericalHistory
 from .form_mixins import SubjectModelFormMixin
 
 
 class ObstericalHistoryForm(SubjectModelFormMixin, forms.ModelForm):
 
-    form_validator_cls = None
+    form_validator_cls = ObstericalHistoryFormValidator
 
     class Meta:
         model = ObstericalHistory
