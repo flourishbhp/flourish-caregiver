ae300aa51f28b3c25fb90102606aa4ad30c5ba4d
ThatoKgamaetsile <thatokgamaetsile@gmail.com>
2021-02-25 10:34:46 +0200

    Add maternal arv during preg model, form, admin

---
+++ flourish_caregiver/admin/maternal_arv_during_preg_admin.py
@@ -0,0 +1,49 @@
+from django.contrib import admin
+from edc_model_admin import TabularInlineMixin
+from edc_model_admin import audit_fieldset_tuple
+
+from ..admin_site import flourish_caregiver_admin
+from ..forms import MaternalArvDuringPregForm, MaternalArvForm
+from ..models import MaternalArvDuringPreg, MaternalArv
+from .modeladmin_mixins import CrfModelAdminMixin
+
+
+class MaternalArvInlineAdmin(TabularInlineMixin, admin.TabularInline):
+    model = MaternalArv
+    form = MaternalArvForm
+    extra = 1
+
+    fieldsets = (
+        (None, {
+            'fields': [
+                'maternal_arv_preg',
+                'arv_code',
+                'start_date',
+                'stop_date',
+                'reason_for_stop',
+                'reason_for_stop_other']}
+         ), audit_fieldset_tuple)
+
+
+@admin.register(MaternalArvDuringPreg, site=flourish_caregiver_admin)
+class MaternalArvDuringPregAdmin(CrfModelAdminMixin, admin.ModelAdmin):
+    form = MaternalArvDuringPregForm
+    inlines = [MaternalArvInlineAdmin, ]
+    list_display = ('maternal_visit', 'took_arv', 'is_interrupt',)
+    list_filter = ('took_arv',)
+
+    fieldsets = (
+        (None, {
+            'fields': [
+                # 'maternal_visit',
+                # 'report_datetime',
+                'took_arv',
+                'is_interrupt',
+                'interrupt',
+                'interrupt_other']}
+         ),)
+
+    radio_fields = {'took_arv': admin.VERTICAL,
+                    'is_interrupt': admin.VERTICAL,
+                    'interrupt': admin.VERTICAL
+                    }
