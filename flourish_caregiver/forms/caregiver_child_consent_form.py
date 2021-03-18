6bbb859a8d4bade2b8c5dfb345acaca3ca063900
ame diphoko <diphokoame@gmail.com>
2021-03-18 09:36:38 +0200

    update caregiver child consent form

--- flourish_caregiver/forms/caregiver_child_consent_form.py
+++ flourish_caregiver/forms/caregiver_child_consent_form.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import ValidationError
+from django import forms
 
 from edc_constants.choices import FEMALE, MALE
 
@@ -8,6 +9,11 @@ from .form_mixins import SubjectModelFormMixin
 
 class CaregiverChildConsentForm(SubjectModelFormMixin):
 
+    subject_identifier = forms.CharField(
+        label='Subject Identifier',
+        widget=forms.TextInput(attrs={'readonly': 'readonly'}),
+        required=False)
+
     def clean(self):
         super().clean()
 
@@ -16,26 +22,25 @@ class CaregiverChildConsentForm(SubjectModelFormMixin):
     def validate_identity_number(self, cleaned_data=None):
         if cleaned_data.get('identity') != cleaned_data.get(
                 'confirm_identity'):
-            msg = {'identity':
-                       '\'Identity\' must match \'confirm identity\'.'}
+            msg = {'identity': '\'Identity\' must match \'confirm identity\'.'}
             self._errors.update(msg)
             raise ValidationError(msg)
         if cleaned_data.get('identity_type') == 'country_id':
             if len(cleaned_data.get('identity')) != 9:
                 msg = {'identity':
-                           'Country identity provided should contain 9 values. '
-                           'Please correct.'}
+                       'Country identity provided should contain 9 values. '
+                       'Please correct.'}
                 self._errors.update(msg)
                 raise ValidationError(msg)
             gender = cleaned_data.get('gender')
             if gender == FEMALE and cleaned_data.get('identity')[4] != '2':
                 msg = {'identity':
-                           'Participant gender is Female. Please correct identity number.'}
+                       'Participant gender is Female. Please correct identity number.'}
                 self._errors.update(msg)
                 raise ValidationError(msg)
             elif gender == MALE and cleaned_data.get('identity')[4] != '1':
                 msg = {'identity':
-                           'Participant is Male. Please correct identity number.'}
+                       'Participant is Male. Please correct identity number.'}
                 self._errors.update(msg)
                 raise ValidationError(msg)
 
