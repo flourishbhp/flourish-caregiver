bc334b8d3943c9ccf12e3d7466ce9a43a39f4238
ckgathi <ckgathi@users.noreply.github.com>
2021-03-19 05:23:55 +0200

    removed pushed conflicts

--- flourish_caregiver/models/model_mixins/referral_form_mixin.py
+++ flourish_caregiver/models/model_mixins/referral_form_mixin.py
@@ -4,11 +4,7 @@ from edc_base.model_fields import OtherCharField
 from ...choices import REFERRED_TO
 
 
-<<<<<<< HEAD
-class ReferralFormMixin:
-=======
 class ReferralFormMixin(models.Model):
->>>>>>> 10658e898b7115019c711a2fe54810d3c80a8eb5
 
     referred_to = models.CharField(
         verbose_name='Referred To',
@@ -18,8 +14,4 @@ class ReferralFormMixin(models.Model):
     referred_to_other = OtherCharField()
 
     class Meta:
-<<<<<<< HEAD
         app_label = 'flourish_caregiver'
-=======
->>>>>>> 10658e898b7115019c711a2fe54810d3c80a8eb5
-        abstract = True
