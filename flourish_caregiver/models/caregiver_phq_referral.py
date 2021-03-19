97e0d8d537933fd0869d832a1264ef928c139202
ThatoKgamaetsile <thatokgamaetsile@gmail.com>
2021-03-19 04:56:41 +0200

    Add caregiver phq referral form

---
+++ flourish_caregiver/models/caregiver_phq_referral.py
@@ -0,0 +1,9 @@
+from .model_mixins import CrfModelMixin, ReferralFormMixin
+
+
+class CaregiverPhqReferral(ReferralFormMixin, CrfModelMixin):
+
+    class Meta(CrfModelMixin.Meta):
+        app_label = 'flourish_caregiver'
+        verbose_name = 'PHQ-9 Referral Form for Caregivers'
+        verbose_name_plural = 'Caregiver Referral'
