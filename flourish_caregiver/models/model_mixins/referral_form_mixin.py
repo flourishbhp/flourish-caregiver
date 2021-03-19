a7eb1d29adbc44379ab7a39ebf1758ed4f4734a5
ThatoKgamaetsile <thatokgamaetsile@gmail.com>
2021-03-19 04:56:03 +0200

    Add referral form mixin

---
+++ flourish_caregiver/models/model_mixins/referral_form_mixin.py
@@ -0,0 +1,18 @@
+from django.db import models
+from edc_base.model_fields import OtherCharField
+
+from ...choices import REFERRED_TO
+
+
+class ReferralFormMixin:
+
+    referred_to = models.CharField(
+        verbose_name='Referred To',
+        choices=REFERRED_TO,
+        max_length=50)
+
+    referred_to_other = OtherCharField()
+
+    class Meta:
+        app_label = 'flourish_caregiver'
+        abstract = True
