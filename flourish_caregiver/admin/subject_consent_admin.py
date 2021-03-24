58ba77dfd8ea4fc782b4965367bb87a9c3048e8b
tumiemosweu <imosweuk@gmail.com>
2021-03-24 12:37:29 +0200

    PEP 8

--- flourish_caregiver/admin/subject_consent_admin.py
+++ flourish_caregiver/admin/subject_consent_admin.py
@@ -217,6 +217,7 @@ class SubjectConsentAdmin(ModelAdminBasicMixin, ModelAdminMixin,
     def get_readonly_fields(self, request, obj=None):
         return (super().get_readonly_fields(request, obj=obj) + audit_fields)
 
+
 @admin.register(CaregiverChildConsent, site=flourish_caregiver_admin)
 class CaregiverChildConsentAdmin(ModelAdminMixin, admin.ModelAdmin):
 
