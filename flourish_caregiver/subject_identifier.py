012685176ba7dd973aac6b102912c1ac860a7115
Coulson Kgathi <ckgathi@gmail.com>
2021-04-13 07:39:28 +0200

    change identifier pattern for bio and non bio mother

--- flourish_caregiver/subject_identifier.py
+++ flourish_caregiver/subject_identifier.py
@@ -6,10 +6,32 @@ class SubjectIdentifier(SubjectIdentifier):
     template = '{protocol_number}-0{site_id}{device_id}{sequence}'
     
     def __init__(self, caregiver_type=None, **kwargs):
-        if caregiver_type:
-            self.template = caregiver_type + self.template
+        self.caregiver_type = caregiver_type
         super().__init__(**kwargs)
 
+    @property
+    def identifier(self):
+        """Returns a new and unique identifier and updates
+        the IdentifierModel.
+        """
+        if not self._identifier:
+            self.pre_identifier()
+            self._identifier = self.template.format(**self.template_opts)
+            check_digit = self.checkdigit.calculate_checkdigit(
+                ''.join(self._identifier.split('-')))
+            if self.caregiver_type:
+                self._identifier = f'{self.caregiver_type}{self._identifier}-{check_digit}'
+            self.identifier_model = self.identifier_model_cls.objects.create(
+                name=self.label,
+                sequence_number=self.sequence_number,
+                identifier=self._identifier,
+                protocol_number=self.protocol_number,
+                device_id=self.device_id,
+                model=self.requesting_model,
+                site=self.site,
+                identifier_type=self.identifier_type)
+            self.post_identifier()
+        return self._identifier
 
 class PreFlourishIdentifier(SubjectIdentifier):
 
---
+++ flourish_caregiver/tests/test_subject_identifier_pattern.py
@@ -0,0 +1,75 @@
+import re
+
+from django.test import TestCase
+from edc_base.utils import get_utcnow
+from edc_facility.import_holidays import import_holidays
+from model_mommy import mommy
+
+from ..models import SubjectConsent
+from edc_constants.constants import NO, YES
+
+subject_identifier = '[B|C]142\-[0-9\-]+'
+
+
+class TestSubjectConsent(TestCase):
+
+    def setUp(self):
+        import_holidays()
+        self.subject_screening = mommy.make_recipe(
+            'flourish_caregiver.screeningpriorbhpparticipants')
+
+        self.eligible_options = {
+            'screening_identifier': self.subject_screening.screening_identifier,
+            'consent_datetime': get_utcnow,
+            'remain_in_study': YES,
+            'hiv_testing': YES,
+            'breastfeed_intent': YES,
+            'consent_reviewed': YES,
+            'study_questions': YES,
+            'assessment_score': YES,
+            'consent_signature': YES,
+            'consent_copy': YES}
+
+
+    def test_subject_identifier_bio_mother(self):
+        """Test consent allocates subject identifier starting with a B for a 
+        biological mother.
+        """
+        self.eligible_options.update(biological_caregiver=YES)
+        mommy.make_recipe('flourish_caregiver.subjectconsent', **self.eligible_options)
+        self.assertTrue(
+            re.match(
+                subject_identifier,
+                SubjectConsent.objects.all()[0].subject_identifier))
+    
+    def test_subject_identifier_bio_mother2(self):
+        """Test consent allocates subject identifier starting with a B for a 
+        biological mother.
+        """
+        self.eligible_options.update(biological_caregiver=YES)
+        mommy.make_recipe('flourish_caregiver.subjectconsent', **self.eligible_options)
+        subject_identifier = SubjectConsent.objects.all()[0].subject_identifier
+        self.assertTrue(subject_identifier.startswith('B'))
+
+    def test_subject_identifier_caregiver(self):
+        """Test consent allocates subject identifier starting with a C for a 
+        non biological mother.
+        """
+        self.eligible_options.update(biological_caregiver=NO)
+        mommy.make_recipe('flourish_caregiver.subjectconsent', **self.eligible_options)
+        self.assertTrue(
+            re.match(
+                subject_identifier,
+                SubjectConsent.objects.all()[0].subject_identifier))
+
+    def test_subject_identifier_caregiver2(self):
+        """Test consent allocates subject identifier starting with a C for a 
+         non biological mother.
+        """
+        self.eligible_options.update(biological_caregiver=NO)
+        mommy.make_recipe('flourish_caregiver.subjectconsent', **self.eligible_options)
+        subject_identifier = SubjectConsent.objects.all()[0].subject_identifier
+        self.assertTrue(subject_identifier.startswith('C'))
+
+
+
