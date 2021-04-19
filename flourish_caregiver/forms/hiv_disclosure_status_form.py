b469838e917eb1572fe34a98c54d24a8be234861
ThatoKgamaetsile <thatokgamaetsile@gmail.com>
2021-04-19 09:47:01 +0200

    Add HIV Disclosure Status Form validator class

--- flourish_caregiver/forms/hiv_disclosure_status_form.py
+++ flourish_caregiver/forms/hiv_disclosure_status_form.py
@@ -3,8 +3,12 @@ from django import forms
 from ..models import HIVDisclosureStatus
 from .form_mixins import SubjectModelFormMixin
 
+from flourish_form_validations.form_validators import HIVDisclosureStatusFormValidator
+
 
 class HIVDisclosureStatusForm(SubjectModelFormMixin, forms.ModelForm):
+
+    form_validator_cls = HIVDisclosureStatusFormValidator
 
     class Meta:
         model = HIVDisclosureStatus
