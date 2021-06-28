c8cc92452ad0f97f48b7c81beb48806519b92574
tumiemosweu <imosweuk@gmail.com>
2021-06-28 16:47:53 +0200

    update maternal dataset search

--- flourish_caregiver/apps.py
+++ flourish_caregiver/apps.py
@@ -10,7 +10,7 @@ class AppConfig(DjangoAppConfig):
     verbose_name = 'Flourish Caregiver'
     admin_site_name = 'flourish_caregiver_admin'
     start_date_year_3 = datetime(
-            2022, 8, 14, 0, 0, 0, tzinfo=gettz('UTC')).date()
+            2022, 7, 1, 0, 0, 0, tzinfo=gettz('UTC')).date()
 
     def ready(self):
         from .models import antenatal_enrollment_on_post_save
