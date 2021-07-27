79873861f6e10c7d2be313a08854d07abe7d27ff
Itumeleng Mosweu <imosweuk@gmail.com>
2021-07-27 16:30:54 +0200

    Revert "add caregiver contact form"
    This reverts commit aa4e97d916083759113f92af449808e315a38e8e.

--- flourish_caregiver/models/__init__.py
+++ flourish_caregiver/models/__init__.py
@@ -4,7 +4,6 @@ from .caregiver_child_consent import CaregiverChildConsent
 from .caregiver_clinical_measurements import CaregiverClinicalMeasurements
 from .caregiver_clinical_measurements_fu import CaregiverClinicalMeasurementsFu
 from .caregiver_clinician_notes import ClinicianNotes, ClinicianNotesImage
-from .caregiver_contact import CaregiverContact
 from .caregiver_edinburgh_depr_screening import CaregiverEdinburghDeprScreening
 from .caregiver_edinburgh_referral import CaregiverEdinburghReferral
 from .caregiver_gad_anxiety_screening import CaregiverGadAnxietyScreening
