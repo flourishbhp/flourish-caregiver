4d4e0f37fa72e289351c4919608d69c57c4a7d7f
Itumeleng Mosweu <imosweuk@gmail.com>
2021-08-09 17:17:19 +0200

    add stem to socio demographic

--- flourish_caregiver/admin/socio_demographic_data_admin.py
+++ flourish_caregiver/admin/socio_demographic_data_admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from edc_fieldsets.fieldlist import Insert
+from edc_fieldsets.fieldsets_modeladmin_mixin import FormLabel
 from edc_model_admin import audit_fieldset_tuple
 
 from ..admin_site import flourish_caregiver_admin
@@ -45,7 +47,31 @@ class SocioDemographicDataAdmin(CrfModelAdminMixin, admin.ModelAdmin):
                     'current_occupation': admin.VERTICAL,
                     'provides_money': admin.VERTICAL,
                     'money_earned': admin.VERTICAL,
-                    'stay_with_child': admin.VERTICAL}
+                    'stay_with_child': admin.VERTICAL,
+                    'socio_demo_changed': admin.VERTICAL}
+
+    custom_form_labels = [
+        FormLabel(
+            field='socio_demo_changed',
+            label=('Since the last time you spoke to a FLOURISH study member, has any of your'
+                   ' following Socio-demographic information changed'),
+            previous_appointment=True)
+        ]
+
+    quartely_schedules = ['a_quarterly1_schedule1', 'a_quarterly2_schedule1',
+                          'a_quarterly3_schedule1', 'a_sec1_schedule1',
+                          'a_sec2_schedule1', 'a_sec3_schedule1',
+                          'b_quarterly1_schedule1', 'b_quarterly2_schedule1',
+                          'b_quarterly3_schedule1', 'c_quarterly2_schedule1',
+                          'c_quarterly1_schedule1', 'c_quarterly3_schedule1',
+                          'b_sec1_schedule1', 'b_sec2_schedule1', 'b_sec3_schedule1',
+                          'c_sec1_schedule1', 'c_sec2_schedule1', 'c_sec3_schedule1',
+                          'pool1_schedule1', 'pool2_schedule1', 'pool3_schedule1']
+
+    conditional_fieldlists = {}
+    for schedule in quartely_schedules:
+        conditional_fieldlists.update(
+            {schedule: Insert('socio_demo_changed', after='report_datetime')})
 
     def get_form(self, request, obj=None, *args, **kwargs):
         form = super().get_form(request, *args, **kwargs)
