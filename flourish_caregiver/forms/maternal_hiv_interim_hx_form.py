c48ebc85e9b110dcd59e6ab1d44709d8bf730c71
Moses Chawawa <moseschawawa@gmail.com>
2021-08-11 16:02:04 +0200

    Added a validation check if the field is just a number

--- flourish_caregiver/forms/maternal_hiv_interim_hx_form.py
+++ flourish_caregiver/forms/maternal_hiv_interim_hx_form.py
@@ -1,13 +1,15 @@
 from django import forms
 from .form_mixins import SubjectModelFormMixin
 from ..models import MaternalHivInterimHx
+from django.core.validators import RegexValidator
 
 from flourish_form_validations.form_validators import MaternalHivInterimHxFormValidator
 
 
 class MaternalHivInterimHxForm(SubjectModelFormMixin, forms.ModelForm):
-
     form_validator_cls = MaternalHivInterimHxFormValidator
+
+    vl_result = forms.CharField(validators=[RegexValidator(r'^[0-9]*$', 'Viral load can only be a number')])
 
     class Meta:
         model = MaternalHivInterimHx
