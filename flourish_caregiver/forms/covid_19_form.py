bbbd3609468c56774e3de5ee8f9ca745aac93717
Moses Chawawa <moseschawawa@gmail.com>
2021-10-29 09:13:27 +0200

    covid form changes

--- flourish_caregiver/forms/covid_19_form.py
+++ flourish_caregiver/forms/covid_19_form.py
@@ -1,11 +1,11 @@
 from .form_mixins import SubjectModelFormMixin
 from django import forms
 from ..models import Covid19
-from flourish_form_validations.form_validators import Covid9FormValidator
+from flourish_form_validations.form_validators import Covid19FormValidator
 
 
 class Covid19Form(SubjectModelFormMixin, forms.ModelForm):
-    form_validator_cls = Covid9FormValidator
+    form_validator_cls = Covid19FormValidator
 
     class Meta:
         model = Covid19
