f104829d5b9545a372a2a6dc5f768cfac07ad2f2
Moses Chawawa <moseschawawa@gmail.com>
2021-11-09 12:52:28 +0200

    added j&j

--- flourish_caregiver/choices.py
+++ flourish_caregiver/choices.py
@@ -616,7 +616,7 @@ ISOLATION_LOCATION = (
 YES_NO_PARTIALLY = (
     (YES, 'Yes'),
     (NO, 'No'),
-    ('partially_jab_or_one_jab', 'Partially / one jab'),
+    ('partially_jab', 'Partially'),
 
 )
 
@@ -624,5 +624,6 @@ VACCINATION_TYPE = (
     ('astrazeneca', 'AstraZeneca'),
     ('sinovac', 'Sinovac'),
     ('pfizer', 'Pfizer'),
+    ('johnson_and_johnson', 'Johnson & Johnson '),
     (OTHER, 'Other')
 )
