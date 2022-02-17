02abe4dd47184996ce78314d40853c00e966b1f5
Moses Chawawa <moseschawawa@gmail.com>
2022-02-17 15:00:06 +0200

    Update eligibility.py

--- flourish_caregiver/models/eligibility.py
+++ flourish_caregiver/models/eligibility.py
@@ -1,6 +1,5 @@
-import imp
 from edc_constants.constants import NO, UNKNOWN, POS
-from ..constants import MAX_GA_LMP_ENROL_WEEKS, MIN_AGE_OF_CONSENT
+from ..constants import MAX_GA_LMP_ENROL_WEEKS, MIN_GA_LMP_ENROL_WEEKS
 
 class AntenatalEnrollmentEligibility:
 
@@ -19,7 +18,7 @@ class AntenatalEnrollmentEligibility:
             self.is_eligible = False
         else:
             lmp_to_use = ga_lmp_enrollment_wks
-            if lmp_to_use and (lmp_to_use < MIN_AGE_OF_CONSENT or lmp_to_use > MAX_GA_LMP_ENROL_WEEKS):
+            if lmp_to_use and (lmp_to_use < MIN_GA_LMP_ENROL_WEEKS or lmp_to_use > MAX_GA_LMP_ENROL_WEEKS):
                 self.error_message.append('Gestation not 16 to 30 weeks.')
                 self.is_eligible = False
             if will_breastfeed == NO:
