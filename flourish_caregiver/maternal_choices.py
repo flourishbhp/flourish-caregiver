74a2f2bac11198e6038f328c170d6761325c2818
McTurner45 <58169969+nmunatsi@users.noreply.github.com>
2022-03-02 12:37:37 +0200

    choices update

--- flourish_caregiver/maternal_choices.py
+++ flourish_caregiver/maternal_choices.py
@@ -509,6 +509,6 @@ REASONS_FOR_RESCHEDULING = (
      'Participant changed mind and asked for a re-appointments or want to withdraw / be-withdraw from participating on study '),
     ('caregiver_not_well', 'Child, mother, caregiver not well'),
     ('undisclosed_personal_reasons', 'Participant has undisclosed personal reasons'),
-    ('undisclosed_personal_reasons', 'Participant has undisclosed personal reasons'),
+    ('another_appointment', 'Participant has another appointment at local clinic/hospital scheduled on the same day'),
 
 )
