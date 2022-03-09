dc6461553519a74d49feb0cee01b09a411b94f54
Moses Chawawa <moseschawawa@gmail.com>
2022-03-09 11:42:28 +0200

    caregiver child consent prefill

--- flourish_caregiver/forms/caregiver_child_consent_form.py
+++ flourish_caregiver/forms/caregiver_child_consent_form.py
@@ -1,3 +1,6 @@
+from functools import partialmethod
+from charset_normalizer import from_path
+
 from django import forms
 from django.apps import apps as django_apps
 
@@ -5,11 +8,19 @@ from ..models import CaregiverChildConsent
 from .form_mixins import SubjectModelFormMixin
 
 from flourish_form_validations.form_validators import CaregiverChildConsentFormValidator
+from edc_constants.constants import MALE, FEMALE
 
 
 class CaregiverChildConsentForm(SubjectModelFormMixin):
 
     form_validator_cls = CaregiverChildConsentFormValidator
+
+    child_dataset_model = 'flourish_child.childdataset'
+
+    @property
+    def child_dataset_cls(self):
+        return django_apps.get_model(self.child_dataset_model)
+
 
     subject_identifier = forms.CharField(
         label='Subject Identifier',
@@ -38,6 +49,47 @@ class CaregiverChildConsentForm(SubjectModelFormMixin):
             return False
         else:
             return True
+
+    def get_formset(self, request, obj=None, **kwargs):
+        initial = []
+        study_maternal_id = request.GET.get('study_maternal_identifier')
+        if study_maternal_id:
+            child_datasets = self.child_dataset_cls.objects.filter(
+                study_maternal_identifier=study_maternal_id)
+            genders = {'Male': MALE, 'Female': FEMALE}
+            if obj:
+                child_datasets = self.get_difference(child_datasets, obj)
+
+            for child in child_datasets:
+                initial.append({
+                    'study_child_identifier': child.study_child_identifier,
+                    'gender': genders.get(child.infant_sex),
+                    'child_dob': child.dob
+                })
+
+        formset = super().get_formset(request, obj=obj, **kwargs)
+        formset.__init__ = partialmethod(formset.__init__, initial=initial)
+        return formset
+
+    def get_extra(self, request, obj=None, **kwargs):
+        extra = super().get_extra(request, obj, **kwargs)
+        study_maternal_id = request.GET.get('study_maternal_identifier')
+        if study_maternal_id:
+            child_datasets = self.child_dataset_cls.objects.filter(
+                study_maternal_identifier=study_maternal_id)
+            if not obj:
+                child_count = child_datasets.count()
+                extra = child_count
+            else:
+                extra = len(self.get_difference(child_datasets, obj))
+        return extra
+
+    def get_difference(self, model_objs, obj=None):
+        cc_ids = obj.caregiverchildconsent_set.values_list(
+            'study_child_identifier', flat=True)
+        return [x for x in model_objs if x.study_child_identifier not in cc_ids]
+
+    
 
     class Meta:
         model = CaregiverChildConsent
