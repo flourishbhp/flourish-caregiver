3d68d907e615e0cd59431bc062e4443aab5d3014
McTurner45 <58169969+nmunatsi@users.noreply.github.com>
2022-04-01 11:22:45 +0200

    Tb study consent and schedule setup

--- flourish_caregiver/consents.py
+++ flourish_caregiver/consents.py
@@ -2,6 +2,7 @@ from django.apps import apps as django_apps
 from edc_consent.consent import Consent
 from edc_consent.site_consents import site_consents
 from edc_constants.constants import MALE, FEMALE
+
 from .consent_object_validator import ConsentObjectValidator
 
 edc_protocol = django_apps.get_app_config('edc_protocol')
@@ -15,6 +16,16 @@ caregiver_v1 = Consent(
     age_is_adult=18,
     age_max=110,
     gender=[MALE, FEMALE])
+
+tb_caregiver_v1 = Consent(
+    'flourish_caregiver.tbinformedconsent',
+    version='1',
+    start=edc_protocol.study_open_datetime,
+    end=edc_protocol.study_close_datetime,
+    age_min=18,
+    age_is_adult=18,
+    age_max=110,
+    gender=[FEMALE])
 
 caregiver_v2 = Consent(
     'flourish_caregiver.subjectconsent',
@@ -30,3 +41,4 @@ site_consents.validator_cls = ConsentObjectValidator
 
 site_consents.register(caregiver_v1)
 site_consents.register(caregiver_v2)
+site_consents.register(tb_caregiver_v1)
