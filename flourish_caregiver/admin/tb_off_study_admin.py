ef752c387dbfe36473dae0cd5756c99934aba804
McTurner45 <58169969+nmunatsi@users.noreply.github.com>
2022-06-29 14:03:57 +0200

    Update tb_off_study_admin.py

--- flourish_caregiver/admin/tb_off_study_admin.py
+++ flourish_caregiver/admin/tb_off_study_admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from edc_model_admin import audit_fieldset_tuple
 
-from .modeladmin_mixins import CrfModelAdminMixin
+from .modeladmin_mixins import ModelAdminMixin
 from ..admin_site import flourish_caregiver_admin
 from ..forms import TbOffStudyForm
 from ..models import TbOffStudy
 
 
 @admin.register(TbOffStudy, site=flourish_caregiver_admin)
-class TbOffStudyAdmin(admin.ModelAdmin):
+class TbOffStudyAdmin(ModelAdminMixin, admin.ModelAdmin):
     form = TbOffStudyForm
 
     search_fields = ('subject_identifier',)
