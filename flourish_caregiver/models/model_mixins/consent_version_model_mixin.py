e2b23582fed7da4d281c50c8ef05b9f0d3576256
Itumeleng Mosweu <imosweuk@gmail.com>
2022-08-08 17:42:09 +0200

    fix consent version filter

--- flourish_caregiver/models/model_mixins/consent_version_model_mixin.py
+++ flourish_caregiver/models/model_mixins/consent_version_model_mixin.py
@@ -23,23 +23,35 @@ class ConsentVersionModelModelMixin:
                 subject_identifier=self.subject_identifier)
         except preg_subject_screening_cls.DoesNotExist:
 
-            try:
-                subject_screening_obj = prior_subject_screening_cls.objects.get(
+            subject_screening_objs = prior_subject_screening_cls.objects.filter(
                     subject_identifier=self.subject_identifier)
-            except prior_subject_screening_cls.DoesNotExist:
+
+            if not subject_screening_objs:
                 raise ValidationError(
                     'Missing Subject Screening form. Please complete '
                     'it before proceeding.')
 
-        if subject_screening_obj:
-            try:
-                consent_version_obj = consent_version_cls.objects.get(
-                    screening_identifier=subject_screening_obj.screening_identifier)
-            except consent_version_cls.DoesNotExist:
+        if subject_screening_objs:
+            screening_identifiers = subject_screening_objs.values_list(
+                'screening_identifier', flat=True)
+        elif subject_screening_obj:
+            screening_identifiers = [subject_screening_obj.screening_identifier, ]
+
+        if screening_identifiers:
+
+            consent_version_obj = consent_version_cls.objects.filter(
+                    screening_identifier__in=screening_identifiers)
+
+            if consent_version_obj.count() > 1:
+                raise ValidationError(
+                    'Multiple Consent Version forms found. Please correct '
+                    'before proceeding.')
+            elif not consent_version_obj:
                 raise ValidationError(
                     'Missing Consent Version form. Please complete '
                     'it before proceeding.')
-            return consent_version_obj.version
+            else:
+                return consent_version_obj[0].version
 
     def save(self, *args, **kwargs):
         self.consent_version = self.get_consent_version()
