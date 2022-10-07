1ea172906096f7c25f332b087e90533cd9493af3
Itumeleng Mosweu <imosweuk@gmail.com>
2022-10-07 13:04:42 +0200

    minor fix

--- flourish_caregiver/forms/tb_interview_form.py
+++ flourish_caregiver/forms/tb_interview_form.py
@@ -1,12 +1,14 @@
 from django import forms
+
+from flourish_form_validations.form_validators import TbInterviewFormValidator
+
 from ..models import TbInterview
 from .form_mixins import SubjectModelFormMixin
 
 
-# from flourish_form_validations.form_validators import TbInterviewFormValidator
 class TbInterviewForm(SubjectModelFormMixin, forms.ModelForm):
 
-    # form_validator_cls = TbRoutineHealthScreenFormValidator
+    form_validator_cls = TbInterviewFormValidator
 
     class Meta:
         model = TbInterview
