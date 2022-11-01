765044822ccaa35f4b2087ed8e00d16de0bb86f3
Itumeleng Mosweu <imosweuk@gmail.com>
2022-11-01 16:18:32 +0200

    update tb adol screening

--- flourish_caregiver/models/tb_adol_screening.py
+++ flourish_caregiver/models/tb_adol_screening.py
@@ -1,19 +1,30 @@
 from django.db import models
 from edc_base.model_fields import OtherCharField
+from edc_base.model_mixins import BaseUuidModel
+from edc_base.model_validators import datetime_not_future
+from edc_base.sites.site_model_mixin import SiteModelMixin
+from edc_base.utils import get_utcnow
 from edc_constants.choices import YES_NO
+from edc_identifier.model_mixins import NonUniqueSubjectIdentifierModelMixin
+from edc_protocol.validators import datetime_not_before_study_start
 
 from flourish_caregiver.choices import REASONS_UNWILLING_ADOL
-from flourish_caregiver.models.model_mixins import CrfModelMixin
 
 
-class TbAdolEligibility(CrfModelMixin):
+class TbAdolEligibility(NonUniqueSubjectIdentifierModelMixin,
+                        SiteModelMixin, BaseUuidModel):
+
+    report_datetime = models.DateTimeField(
+        verbose_name='Report Time and Date',
+        default=get_utcnow,
+        validators=[datetime_not_future, datetime_not_before_study_start],)
 
     tb_adol_participation = models.CharField(
         verbose_name=('Participant willing to do an Informed consent for the '
                       'TB Adolescent Study'),
-        help_text='Illegible for TB adolescent study if NO',
         choices=YES_NO,
-        max_length=10,)
+        max_length=10,
+        help_text='Elligible for TB adolescent study if NO')
 
     reasons_unwilling_part = models.CharField(
         verbose_name='Reasons unable to obtain an informed consent for TB study',
