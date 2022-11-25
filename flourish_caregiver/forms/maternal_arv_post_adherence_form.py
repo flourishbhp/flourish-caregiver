2d78c4fc62e36496f8ea4fbd41a15e1db1f6e5b9
Itumeleng Mosweu <imosweuk@gmail.com>
2022-11-25 10:31:37 +0200

    Update maternal_arv_post_adherence_form.py

--- flourish_caregiver/forms/maternal_arv_post_adherence_form.py
+++ flourish_caregiver/forms/maternal_arv_post_adherence_form.py
@@ -2,11 +2,11 @@ from django import forms
 from ..models import MaternalArvPostAdherence
 from .form_mixins import SubjectModelFormMixin
 
-#from flourish_form_validations.form_validators import MaternalArvPostAdherenceFormValidator
+from flourish_form_validations.form_validators import MaternalArvPostAdherenceFormValidator
 
 
 class MaternalArvPostAdherenceForm(SubjectModelFormMixin, forms.ModelForm):
-    #form_validator_cls = MaternalArvPostAdherenceFormValidator
+    form_validator_cls = MaternalArvPostAdherenceFormValidator
 
     class Meta:
         model = MaternalArvPostAdherence
