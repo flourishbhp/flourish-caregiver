d011f2f8fe3942bab977394ddfc21a476174b027
Itumeleng Mosweu <imosweuk@gmail.com>
2022-12-01 17:07:04 +0200

    update referral forms

--- flourish_caregiver/models/caregiver_phq_referral_fu.py
+++ flourish_caregiver/models/caregiver_phq_referral_fu.py
@@ -11,9 +11,7 @@ class CaregiverPhqReferralFU(ReferralFUFormMixin, CrfModelMixin):
                       'support services. Do you mind sharing with us where you are receiving '
                       'these services?'),
         max_length=40,
-        choices=EMO_SUPPORT_PROVIDER,
-        blank=True,
-        null=True)
+        choices=EMO_SUPPORT_PROVIDER)
 
     class Meta(CrfModelMixin.Meta):
         app_label = 'flourish_caregiver'
