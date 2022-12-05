01672ff02f1868746d2d5f4614624d8bce306e63
salahdin <salahdinjemal@gmail.com>
2022-12-05 10:23:48 +0200

    enable null on further_tb_eval

--- flourish_caregiver/models/tb_referral_outcomes.py
+++ flourish_caregiver/models/tb_referral_outcomes.py
@@ -18,7 +18,10 @@ class TbReferralOutcomes(CrfModelMixin):
         verbose_name=('Did the participant go to any clinic for further TB evaluation '
                       'after they were referred?'),
         max_length=3,
-        choices=YES_NO)
+        choices=YES_NO,
+        null=True,
+        blank=True
+    )
 
     tb_eval_comments = models.TextField(
         verbose_name=('Comments'),
