4f008f521573f71e8fafeaab75d10e1f176f2363
salahdin <salahdinjemal@gmail.com>
2023-01-10 08:37:18 +0200

    remove unused imports

--- flourish_caregiver/forms/tb_routine_health_screen_v2_form.py
+++ flourish_caregiver/forms/tb_routine_health_screen_v2_form.py
@@ -3,9 +3,8 @@ from django.apps import apps as django_apps
 from django.core.exceptions import ValidationError
 from flourish_form_validations.form_validators import TbRoutineHealthScreenV2FormValidator
 
-from ..models import TbRoutineHealthScreenV2, TbRoutineHealthEncounters, MaternalVisit
+from ..models import TbRoutineHealthScreenV2, TbRoutineHealthEncounters
 from .form_mixins import SubjectModelFormMixin, InlineSubjectModelFormMixin
-from ..choices import YES_NO_UNK_DWTA, VISIT_NUMBER
 
 
 class TbRoutineHealthScreenV2Form(SubjectModelFormMixin, forms.ModelForm):
