ae6d6870a0a5b2245694507134e55bd6412197e0
ame diphoko <diphokoame@gmail.com>
2023-04-13 14:18:02 +0200

    fix: remove requisition results imports

--- flourish_caregiver/models/__init__.py
+++ flourish_caregiver/models/__init__.py
@@ -25,8 +25,6 @@ from .caregiver_phq_referral import CaregiverPhqReferral
 from .caregiver_phq_referral_fu import CaregiverPhqReferralFU
 from .caregiver_previously_enrolled import CaregiverPreviouslyEnrolled
 from .caregiver_requisition import CaregiverRequisition
-from .caregiver_requisition_result import CaregiverRequisitionResult
-from .caregiver_requisition_result import CaregiverResultValue
 from .caregiver_social_work_referral import CaregiverSocialWorkReferral
 from .covid_19 import Covid19
 from .enrollment import Enrollment
