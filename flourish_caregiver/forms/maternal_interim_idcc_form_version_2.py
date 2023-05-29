2414fc95f46272463d824bee815d27aef01990d7
Moses Chawawa <moseschawawa@gmail.com>
2023-05-29 12:01:16 +0200

    Added extra fields

--- flourish_caregiver/forms/maternal_interim_idcc_form_version_2.py
+++ flourish_caregiver/forms/maternal_interim_idcc_form_version_2.py
@@ -2,12 +2,12 @@ from django import forms
 from ..models import MaternalInterimIdccVersion2
 from .form_mixins import SubjectModelFormMixin
 
-from flourish_form_validations.form_validators import MaternalIterimIdccFormValidator
+from flourish_form_validations.form_validators import MaternalIterimIdccFormVersion2Validator
 
 
 class MaternalInterimIdccFormVersion2(SubjectModelFormMixin, forms.ModelForm):
 
-    form_validator_cls = MaternalIterimIdccFormValidator
+    form_validator_cls = MaternalIterimIdccFormVersion2Validator
 
     class Meta:
         model = MaternalInterimIdccVersion2
