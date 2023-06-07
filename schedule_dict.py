ef07ab2459dcfce9412478aead6f176f313ef009
nmunatsibw <mcturnergarai@gmail.com>
2023-06-07 13:38:08 +0200

    :sparkles: feat(Sequential enrolment): put a sequential enrolment participant into a sequential scheduled

---
+++ schedule_dict.py
@@ -0,0 +1,257 @@
+schedule_dict = {
+    'cohort_a': {
+        'antenatal': {
+            'onschedulecohortaantenatal': {
+                1: 'a_antenatal1_schedule1',
+                2: 'a_antenatal2_schedule1',
+                3: 'a_antenatal3_schedule1',
+            }
+        },
+        'birth': {
+            'onschedulecohortabirth': {
+                1: 'a_birth1_schedule1',
+                2: 'a_birth2_schedule1',
+                3: 'a_birth3_schedule1',
+            }
+        },
+        'enrollment': {
+            'onschedulecohortaenrollment': {
+                1: 'a_enrol1_schedule1',
+                2: 'a_enrol2_schedule1',
+                3: 'a_enrol3_schedule1',
+            }
+        },
+        'followup': {
+            'onschedulecohortafu': {
+                1: 'a_fu1_schedule1',
+                2: 'a_fu2_schedule1',
+                3: 'a_fu3_schedule1',
+            }
+        },
+        'followup_quartaly': {
+            'onschedulecohortafu': {
+                1: 'a_fu1_schedule1',
+                2: 'a_fu2_schedule1',
+                3: 'a_fu3_schedule1',
+            }
+        },
+        'quarterly': {
+            'onschedulecohortaquarterly': {
+                1: 'a_quarterly1_schedule1',
+                2: 'a_quarterly2_schedule1',
+                3: 'a_quarterly3_schedule1',
+            }
+        },
+        'sec_aims_enrol': {
+            'onschedulecohortase': {
+                1: 'a_sec1_schedule1',
+                2: 'a_sec2_schedule1',
+                3: 'a_sec3_schedule1',
+            }
+        },
+        'sec_aims_quart': {
+            'onschedulecohortasecquart': {
+                1: 'a_sec_quart1_schedule1',
+                2: 'a_sec_quart2_schedule1',
+                3: 'a_sec_quart3_schedule1',
+            }
+        },
+        'tb_2_months': {
+            'onschedulecohortatb2months': {
+                1: 'a_tb1_2_months_schedule1',
+                2: 'a_tb2_2_months_schedule1',
+                3: 'a_tb3_2_months_schedule1',
+            }
+        },
+        'tb_6_months': {
+            'onschedulecohortatb6months': {
+                1: 'a_tb1_6_months_schedule1',
+                2: 'a_tb2_6_months_schedule1',
+                3: 'a_tb3_6_months_schedule1',
+            }
+        },
+    },
+    'cohort_b': {
+        'enrollment': {
+            'onschedulecohortbenrollment': {
+                1: 'b_enrol1_schedule1',
+                2: 'b_enrol2_schedule1',
+                3: 'b_enrol3_schedule1',
+            }
+        },
+        'followup': {
+            'onschedulecohortbfu': {
+                1: 'b_fu1_schedule1',
+                2: 'b_fu2_schedule1',
+                3: 'b_fu3_schedule1',
+            }
+        },
+        'followup_quartaly': {
+            'onschedulecohortbfuquarterly': {
+                1: 'b_fu1_schedule1',
+                2: 'b_fu2_schedule1',
+                3: 'b_fu3_schedule1',
+            }
+        },
+        'quarterly': {
+            'onschedulecohortbquarterly': {
+                1: 'b_quarterly1_schedule1',
+                2: 'b_quarterly2_schedule1',
+                3: 'b_quarterly3_schedule1',
+            }
+        },
+        'sec_aims_enrol': {
+            'onschedulecohortbsec': {
+                1: 'b_sec1_schedule1',
+                2: 'b_sec2_schedule1',
+                3: 'b_sec3_schedule1',
+            }
+        },
+        'sec_aims_quart': {
+            'onschedulecohortbsecquart': {
+                1: 'b_sec_quart1_schedule1',
+                2: 'b_sec_quart2_schedule1',
+                3: 'b_sec_quart3_schedule1',
+            }
+        },
+    },
+    'cohort_c': {
+        'enrollment': {
+            'onschedulecohortcenrollment': {
+                1: 'c_enrol1_schedule1',
+                2: 'c_enrol2_schedule1',
+                3: 'c_enrol3_schedule1',
+            }
+        },
+        'followup': {
+            'onschedulecohortcfu': {
+                1: 'c_fu1_schedule1',
+                2: 'c_fu2_schedule1',
+                3: 'c_fu3_schedule1',
+            }
+        },
+        'followup_quartaly': {
+            'onschedulecohortcfuquarterly': {
+                1: 'c_fu1_schedule1',
+                2: 'c_fu2_schedule1',
+                3: 'c_fu3_schedule1',
+            }
+        },
+        'quarterly': {
+            'onschedulecohortcquarterly': {
+                1: 'c_quarterly1_schedule1',
+                2: 'c_quarterly2_schedule1',
+                3: 'c_quarterly3_schedule1',
+            }
+        },
+        'sec_aims_enrol': {
+            'onschedulecohortcsec': {
+                1: 'c_sec1_schedule1',
+                2: 'c_sec2_schedule1',
+                3: 'c_sec3_schedule1',
+            }
+        },
+        'sec_aims_quart': {
+            'onschedulecohortcsecquart': {
+                1: 'c_sec_quart1_schedule1',
+                2: 'c_sec_quart2_schedule1',
+                3: 'c_sec_quart3_schedule1',
+            }
+        },
+    },
+}
+
+child_schedule_dict = {
+    'cohort_a': {
+        'birth': {
+            'onschedule_model': 'onschedulechildcohortabirth',
+            'name': 'child_a_birth_schedule1',
+        },
+        'enrollment': {
+            'onschedule_model': 'onschedulechildcohortaenrollment',
+            'name': 'child_a_enrol_schedule1',
+        },
+        'followup': {
+            'onschedule_model': 'onschedulechildcohortafu',
+            'name': 'child_a_fu_schedule1',
+        },
+        'followup_quartaly': {
+            'onschedule_model': 'onschedulechildcohortafuquart',
+            'name': 'child_a_fu_qt_schedule1',
+        },
+        'quarterly': {
+            'onschedule_model': 'onschedulechildcohortaquarterly',
+            'name': 'child_a_quart_schedule1',
+        },
+        'sec_aims_enrol': {
+            'onschedule_model': 'onschedulechildcohortasec',
+            'name': 'child_a_sec_schedule1',
+        },
+        'sec_aims_quart': {
+            'onschedule_model': 'onschedulechildcohortasecquart',
+            'name': 'child_a_sec_qt_schedule1',
+        },
+    },
+    'cohort_b': {
+        'enrollment': {
+            'onschedule_model': 'onschedulechildcohortbenrollment',
+            'name': 'child_b_enrol_schedule1',
+        },
+        'followup': {
+            'onschedule_model': 'onschedulechildcohortbfu',
+            'name': 'child_b_fu_schedule1',
+        },
+        'followup_quartaly': {
+            'onschedule_model': 'onschedulechildcohortbfuquart',
+            'name': 'child_b_fu_qt_schedule1',
+        },
+        'quarterly': {
+            'onschedule_model': 'onschedulechildcohortbquarterly',
+            'name': 'child_b_quart_schedule1',
+        },
+        'sec_aims_enrol': {
+            'onschedule_model': 'onschedulechildcohortbsec',
+            'name': 'child_b_sec_schedule1',
+        },
+        'sec_aims_quart': {
+            'onschedule_model': 'onschedulechildcohortbsecquart',
+            'name': 'child_b_sec_qt_schedule1',
+        },
+    },
+    'cohort_c': {
+        'enrollment': {
+            'onschedule_model': 'onschedulechildcohortcenrollment',
+            'name': 'child_c_enrol_schedule1',
+        },
+        'followup': {
+            'onschedule_model': 'onschedulechildcohortcfu',
+            'name': 'child_c_fu_schedule1',
+        },
+        'followup_quartaly': {
+            'onschedule_model': 'onschedulechildcohortcfuquart',
+            'name': 'child_c_fu_qt_schedule1',
+        },
+        'quarterly': {
+            'onschedule_model': 'onschedulechildcohortcquarterly',
+            'name': 'child_c_quart_schedule1',
+        },
+        'sec_aims_enrol': {
+            'onschedule_model': 'onschedulechildcohortcsec',
+            'name': 'child_c_sec_schedule1',
+        },
+        'sec_aims_quart': {
+            'onschedule_model': 'onschedulechildcohortcsecquart',
+            'name': 'child_c_sec_qt_schedule1',
+        },
+    },
+    'tb': {
+        'adolescent': {
+            'onschedule_model': 'onschedulechildtbadolschedule',
+            'name': 'tb_adol_schedule',
+        },
+        'followup': {
+            'onschedule_model': 'onscheduletbadolfollowupschedule',
+            'name': 'tb_adol_followup_schedule',
+        },
+    },
+}
