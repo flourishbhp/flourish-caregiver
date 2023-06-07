e5dc945877f05622c4893d89ea96e3354d6630a6
Moses Chawawa <moseschawawa@gmail.com>
2023-06-07 16:15:23 +0200

    Refactor

--- flourish_caregiver/helper_classes/seq_enrol_onschedule_mixin.py
+++ flourish_caregiver/helper_classes/seq_enrol_onschedule_mixin.py
@@ -2,7 +2,7 @@ from django.apps import apps as django_apps
 from edc_base import get_utcnow
 from edc_visit_schedule import site_visit_schedules
 
-from schedule_dict import child_schedule_dict, schedule_dict
+from schedule_dict import child_schedule_dict, caregiver_schedule_dict
 
 
 class SeqEnrolOnScheduleMixin:
@@ -11,28 +11,31 @@ class SeqEnrolOnScheduleMixin:
         """
         # TODO evaluate cohort
         cohort = self.evaluated_cohort
-        onschedule_model = next(iter(schedule_dict[cohort][schedule_type]))
-        schedule_name = schedule_dict[cohort][schedule_type][onschedule_model][
-            child_count]
-        onschedule_model = f'flourish_caregiver.{onschedule_model}'
+        schedule_type = schedule_type or self.current_quartarly_schedule_type
+        onschedule_model = caregiver_schedule_dict[cohort][schedule_type]['onschedule_model']
+        schedule_name = caregiver_schedule_dict[cohort][schedule_type][child_count]
+
         self.put_on_schedule(onschedule_model=onschedule_model,
-                             schedule_name=schedule_name)
+                             schedule_name=schedule_name,
+                             subject_identifier=self.caregiver_subject_identifier)
 
     def put_child_onschedule(self, schedule_type):
         # TODO evaluate cohort
         cohort = self.evaluated_cohort
-        onschedule_model = schedule_dict[cohort][schedule_type]['onschedule_model']
+        schedule_type = schedule_type or self.current_quartarly_schedule_type
+        onschedule_model = child_schedule_dict[cohort][schedule_type]['onschedule_model']
         schedule_name = child_schedule_dict[cohort][schedule_type]['name']
-        onschedule_model = f'flourish_child.{onschedule_model}'
-        self.put_on_schedule(onschedule_model=onschedule_model,
-                             schedule_name=schedule_name)
 
-    def put_on_schedule(self, onschedule_model, schedule_name):
+        self.put_on_schedule(onschedule_model=onschedule_model,
+                             schedule_name=schedule_name,
+                             subject_identifier=self.child_subject_identifier)
+
+    def put_on_schedule(self, onschedule_model, schedule_name, subject_identifier):
         _, schedule = site_visit_schedules.get_by_onschedule_model_schedule_name(
             onschedule_model=onschedule_model,
             name=schedule_name)
         schedule.put_on_schedule(
-            subject_identifier=self.caregiver_subject_identifier,
+            subject_identifier=subject_identifier,
             onschedule_datetime=get_utcnow(),
             schedule_name=schedule_name)
 
