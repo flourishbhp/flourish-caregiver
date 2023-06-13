df2dbc93728ea30c5fb64ee51c7e7ab82ea910bc
nmunatsibw <mcturnergarai@gmail.com>
2023-06-13 15:33:47 +0200

    :sparkles: feat(remove completed appointments in sequential enrolment):

--- flourish_caregiver/helper_classes/sequential_onschedule_mixin.py
+++ flourish_caregiver/helper_classes/sequential_onschedule_mixin.py
@@ -1,4 +1,5 @@
 from django.apps import apps as django_apps
+from edc_appointment.models import Appointment
 from edc_base import get_utcnow
 from edc_visit_schedule import site_visit_schedules
 
@@ -6,6 +7,11 @@ from schedule_dict import child_schedule_dict, caregiver_schedule_dict
 
 
 class SeqEnrolOnScheduleMixin:
+
+    @property
+    def child_appointment_cls(self):
+        return django_apps.get_model('flourish_child.child_appointment')
+
     def put_caregiver_onschedule(self):
         """Put a caregiver on schedule.
         """
@@ -13,22 +19,28 @@ class SeqEnrolOnScheduleMixin:
         schedule_type = self.schedule_type
         child_count = str(self.child_consent_obj.caregiver_visit_count)
 
-        onschedule_model = caregiver_schedule_dict[cohort][schedule_type]['onschedule_model']
+        onschedule_model = caregiver_schedule_dict[cohort][schedule_type][
+            'onschedule_model']
         schedule_name = caregiver_schedule_dict[cohort][schedule_type][child_count]
 
         _, schedule = site_visit_schedules.get_by_onschedule_model_schedule_name(
             onschedule_model=onschedule_model,
             name=schedule_name)
 
-        print(onschedule_model)
-        print(schedule_name)
-
         self.put_on_schedule(onschedule_model=onschedule_model,
                              schedule_name=schedule_name,
                              subject_identifier=self.caregiver_subject_identifier)
 
         self.update_onschedule_model(onschedule_model=onschedule_model,
-                                     schedule_name=schedule_name,)
+                                     schedule_name=schedule_name, )
+
+        prev_schedule_name = ''
+
+        self.delete_completed_appointments(
+            appointment_model_cls=Appointment,
+            subject_identifier=self.caregiver_subject_identifier,
+            prev_schedule_name=prev_schedule_name,
+            new_schedule_name=schedule_name)
 
     def put_child_onschedule(self):
         cohort = self.evaluated_cohort
@@ -42,10 +54,17 @@ class SeqEnrolOnScheduleMixin:
 
         if not schedule.is_onschedule(subject_identifier=self.child_subject_identifier,
                                       report_datetime=self.child_consent_obj.consent_datetime):
-
             self.put_on_schedule(onschedule_model=onschedule_model,
                                  schedule_name=schedule_name,
                                  subject_identifier=self.child_subject_identifier)
+
+            prev_schedule_name = ''
+
+            self.delete_completed_appointments(
+                appointment_model_cls=self.child_appointment_cls,
+                subject_identifier=self.child_subject_identifier,
+                prev_schedule_name=prev_schedule_name,
+                new_schedule_name=schedule_name)
 
     def put_on_schedule(self, onschedule_model, schedule_name, subject_identifier):
         _, schedule = site_visit_schedules.get_by_onschedule_model_schedule_name(
@@ -74,3 +93,33 @@ class SeqEnrolOnScheduleMixin:
             else:
                 onschedule_obj.child_subject_identifier = self.child_subject_identifier
                 onschedule_obj.save()
+
+    @classmethod
+    def delete_completed_appointments(cls, appointment_model_cls, subject_identifier,
+                                      prev_schedule_name, new_schedule_name):
+        """Deletes completed appointments from previous schedules which are present in
+        new schedules.
+
+        Args:
+            appointment_model_cls (Type[Appointment]): Model class for Appointments.
+            subject_identifier (str): Unique identifier for the subject.
+            prev_schedule_name (str): Name of the previous schedule.
+            new_schedule_name (str): Name of the new schedule.
+
+        Returns:
+            None
+        """
+        prev_appts = appointment_model_cls.objects.filter(
+            subject_identifier=subject_identifier,
+            schedule_name=prev_schedule_name
+        )
+
+        new_appts = appointment_model_cls.objects.filter(
+            subject_identifier=subject_identifier,
+            schedule_name=new_schedule_name
+        )
+        new_visit_codes = {appt.visit_code for appt in new_appts}
+        for prev_appt in prev_appts:
+            if prev_appt.visit_code in new_visit_codes:
+                new_appt = new_appts.get(visit_code=prev_appt.visit_code)
+                new_appt.delete()
--- flourish_caregiver/helper_classes/sequential_subject_helper.py
+++ flourish_caregiver/helper_classes/sequential_subject_helper.py
@@ -2,7 +2,12 @@ from dateutil.relativedelta import relativedelta
 from django.apps import apps as django_apps
 from model_mommy import mommy
 
+from flourish_caregiver.models import CaregiverChildConsent, \
+    CaregiverPreviouslyEnrolled, \
+    MaternalDataset, \
+    SubjectConsent
 from flourish_caregiver.subject_helper_mixin import SubjectHelperMixin
+from flourish_child.models import ChildDataset
 
 
 class SequentialSubjectHelper:
@@ -31,18 +36,96 @@ class SequentialSubjectHelper:
         return child_dob
 
     def enrollment_and_cohort_assignment(self,
-            protocol=None, age_year=None, age_month=None, efv=None,
-            subject_identifier=None):
+                                         protocol=None, age_year=None, age_month=None,
+                                         efv=None,
+                                         dob=None, screening_identifier=None,
+                                         subject_identifier=None, create_dataset=True):
         """Creates a 5.1-year-old by year 3 participant's mother who is on
         efv regimen from Mpepu study is put on cohort b schedule.
 
         Returns:
         - The subject identifier of the participant's child.
         """
+        if create_dataset:
+            maternal_dataset_obj = self.create_dataset(
+                subject_identifier=subject_identifier,
+                protocol=protocol, dob=dob, efv=efv)
+
+            sh = SubjectHelperMixin()
+
+            _subject_identifier = sh.enroll_prior_participant(
+                maternal_dataset_obj.screening_identifier,
+                study_child_identifier=self.child_dataset_options[
+                    'study_child_identifier'])
+
+        else:
+            _subject_identifier = self.update_dataset(
+                subject_identifier=subject_identifier, dob=dob)
+
+        return _subject_identifier
+
+    def update_dataset(self, subject_identifier, dob):
+        try:
+            maternal_dataset_obj = MaternalDataset.objects.get(
+                subject_identifier=subject_identifier)
+        except MaternalDataset.DoesNotExist:
+            pass
+        else:
+            try:
+                child_dataset_obj = ChildDataset.objects.get(
+                    study_maternal_identifier=maternal_dataset_obj
+                    .study_maternal_identifier)
+            except ChildDataset.DoesNotExist:
+                pass
+            else:
+                child_dataset_obj.dob = dob
+                child_dataset_obj.save()
+            maternal_dataset_obj.delivdt = dob
+            maternal_dataset_obj.save()
+            subject_consent = self.update_consent(
+                subject_identifier=subject_identifier)
+            self.update_caregiver_child_consent(subject_consent=subject_consent, dob=dob)
+            self.resave_caregiver_previously_enrolled(
+                subject_identifier=subject_identifier)
+            return subject_identifier
+
+    def resave_caregiver_previously_enrolled(self, subject_identifier):
+        try:
+            caregiver_previously_enrolled_obj = CaregiverPreviouslyEnrolled.objects.get(
+                subject_identifier=subject_identifier
+            )
+        except CaregiverPreviouslyEnrolled.DoesNotExist:
+            pass
+        else:
+            caregiver_previously_enrolled_obj.save()
+
+    def update_caregiver_child_consent(self, subject_consent, dob):
+        try:
+            caregiver_child_consent_obj = CaregiverChildConsent.objects.get(
+                subject_consent=subject_consent
+            )
+        except CaregiverChildConsent.DoesNotExist:
+            pass
+        else:
+            caregiver_child_consent_obj.child_dob = dob
+            caregiver_child_consent_obj.save()
+            return caregiver_child_consent_obj
+
+    def update_consent(self, subject_identifier):
+        try:
+            subject_consent = SubjectConsent.objects.get(
+                subject_identifier=subject_identifier
+            )
+        except SubjectConsent.DoesNotExist:
+            pass
+        else:
+            return subject_consent
+
+    def create_dataset(self, subject_identifier, protocol, dob, efv):
         self.subject_identifier = subject_identifier
         self.study_maternal_identifier = '981232'
         self.maternal_dataset_options['protocol'] = protocol
-        self.maternal_dataset_options['delivdt'] = self.year_3_age(age_year, age_month)
+        self.maternal_dataset_options['delivdt'] = dob
 
         maternal_dataset_obj = mommy.make_recipe(
             'flourish_caregiver.maternaldataset',
@@ -52,36 +135,36 @@ class SequentialSubjectHelper:
 
         mommy.make_recipe(
             'flourish_child.childdataset',
-            dob=self.year_3_age(age_year, age_month),
+            dob=dob,
             **self.child_dataset_options)
 
-        sh = SubjectHelperMixin()
-
-        _subject_identifier = sh.enroll_prior_participant(
-            maternal_dataset_obj.screening_identifier,
-            study_child_identifier=self.child_dataset_options['study_child_identifier'])
-
-        return _subject_identifier
+        return maternal_dataset_obj
 
     def get_cohort_a_subj(self):
+        dob = self.year_3_age(4, 1)
         return self.enrollment_and_cohort_assignment(
             protocol='Tshilo Dikotla',
             age_year=4,
             age_month=1,
-            subject_identifier='12345678'
+            subject_identifier='12345678',
+            dob=dob
         )
 
     def get_cohort_b_subj(self):
+        dob = self.year_3_age(6, 1)
         return self.enrollment_and_cohort_assignment(
             protocol='Mpepu',
             age_year=6,
             age_month=1,
             efv=1,
+            dob=dob,
             subject_identifier='12345678'
         )
 
     def get_cohort_c_subj(self):
+        dob = self.year_3_age(11, 1)
         return self.enrollment_and_cohort_assignment(
             protocol='Tshipidi', age_year=11,
             age_month=1,
+            dob=dob,
             subject_identifier='12345678')
