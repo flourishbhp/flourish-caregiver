f4d4be1969b23b4f67b165ec80c16a8d1a9d7a7f
nmunatsibw <mcturnergarai@gmail.com>
2023-06-14 15:15:06 +0200

    :sparkles: feat(cohort schedule ): add cohort schedules class

--- flourish_caregiver/models/cohort_schedules.py
+++ flourish_caregiver/models/cohort_schedules.py
@@ -1,15 +1,47 @@
 from django.db import models
 from edc_base.model_managers import HistoricalRecords
 from edc_base.model_mixins import BaseUuidModel
-from edc_base.model_validators import datetime_not_future
 from edc_base.sites import SiteModelMixin
-from edc_base.utils import get_utcnow
-from edc_constants.choices import YES_NO
-from edc_protocol.validators import datetime_not_before_study_start
-from edc_visit_schedule.models import SubjectScheduleHistory
+
 from .model_mixins import SearchSlugModelMixin
 
 
-class CohortSchedules(SubjectScheduleHistory):
+class CohortSchedules(SiteModelMixin, SearchSlugModelMixin, BaseUuidModel):
     """ A model completed by the system for cohort assignment.
     """
+
+    schedule_name = models.CharField(
+        max_length=50,
+        verbose_name="Schedule name",
+    )
+
+    schedule_type = models.CharField(
+        max_length=50,
+        verbose_name="Schedule type",
+    )
+
+    cohort_name = models.CharField(
+        max_length=50,
+        verbose_name="Cohort name",
+    )
+
+    onschedule_model = models.CharField(
+        max_length=100,
+        verbose_name="OnSchedule model",
+    )
+
+    child_count = models.IntegerField(
+        verbose_name="Child count",
+        null=True
+    )
+
+    def get_search_slug_fields(self):
+        fields = ['schedule_name']
+        return fields
+
+    history = HistoricalRecords()
+
+    class Meta:
+        app_label = 'flourish_caregiver'
+        verbose_name = "Cohort Schedule"
+        verbose_name_plural = "Cohort Schedules"
