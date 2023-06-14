58160043adf65604bcf99341ca8c4e9b409ffbd3
Moses Chawawa <moseschawawa@gmail.com>
2023-06-14 23:26:13 +0200

    fix for the off study schedule

--- flourish_caregiver/helper_classes/sequential_offschedule_mixin.py
+++ flourish_caregiver/helper_classes/sequential_offschedule_mixin.py
@@ -6,33 +6,33 @@ class OffScheduleSequentialCohortEnrollmentMixin:
 
     def take_off_child_offschedule(self):
 
+        schedule_name = self.child_last_qt_subject_schedule_obj.schedule_name
+        onschedule_model = self.child_last_qt_subject_schedule_obj.onschedule_model
+
         _, schedule = site_visit_schedules.get_by_onschedule_model_schedule_name(
-            onschedule_model=self.child_last_qt_subject_schedule_obj.onschedule_model,
-            name=self.child_last_qt_subject_schedule_obj.onschedule_model)
+            onschedule_model=onschedule_model,
+            name=schedule_name)
 
         if schedule.is_onschedule(subject_identifier=self.child_subject_identifier,
                                   report_datetime=self.child_consent_obj.consent_datetime):
             schedule.take_off_schedule(
                 subject_identifier=self.child_subject_identifier,
-                schedule_name=self.schedule_name)
+                schedule_name=schedule_name)
 
     def take_off_caregiver_offschedule(self):
 
-        cohort = self.evaluated_cohort
-        schedule_type = self.schedule_type
-        child_count = self.child_count
-
-        schedule_name = caregiver_schedule_dict[cohort][schedule_type][child_count]
+        schedule_name = self.caregiver_last_qt_subject_schedule_obj.schedule_name
+        onschedule_model = self.caregiver_last_qt_subject_schedule_obj.onschedule_model
 
         _, schedule = site_visit_schedules.get_by_onschedule_model_schedule_name(
-            onschedule_model=self.onschedule_model,
-            name=self.schedule_name)
+            onschedule_model=onschedule_model,
+            name=schedule_name)
 
         if schedule.is_onschedule(subject_identifier=self.caregiver_subject_identifier,
                                   report_datetime=self.child_consent_obj.consent_datetime):
             schedule.take_off_schedule(
                 subject_identifier=self.caregiver_subject_identifier,
-                schedule_name=self.schedule_name)
+                schedule_name=schedule_name)
 
     def take_off_schedule(self):
         self.take_off_caregiver_offschedule()
