e0699b3245118228f87a3113be576ab73ecfed18
Ame Diphoko <diphokoame@gmail.com>
2023-07-28 15:30:32 +0200

    fix: use correct cohort classification class

--- flourish_caregiver/helper_classes/utils.py
+++ flourish_caregiver/helper_classes/utils.py
@@ -1,14 +1,14 @@
 from django.apps import apps as django_apps
 
-from flourish_caregiver.helper_classes.cohort import Cohort
-from flourish_caregiver.models.maternal_dataset import MaternalDataset
+from ..helper_classes.cohort_assignment import CohortAssignment
+from ..models.maternal_dataset import MaternalDataset
 
 
 def cohort_assigned(study_child_identifier, child_dob, enrollment_date):
     """Calculates participant's cohort based on the maternal and child dataset
     """
     infant_dataset_cls = django_apps.get_model('flourish_child.childdataset')
-
+    infant_dataset_obj = None
     try:
         infant_dataset_obj = infant_dataset_cls.objects.get(
             study_child_identifier=study_child_identifier,
@@ -19,21 +19,19 @@ def cohort_assigned(study_child_identifier, child_dob, enrollment_date):
         infant_dataset_obj = infant_dataset_cls.objects.filter(
             study_child_identifier=study_child_identifier,
             dob=child_dob)[0]
-    else:
+    finally:
         try:
             maternal_dataset_obj = MaternalDataset.objects.get(
-                study_maternal_identifier=infant_dataset_obj.study_maternal_identifier)
+                study_maternal_identifier=getattr(
+                    infant_dataset_obj, 'study_maternal_identifier', None))
         except MaternalDataset.DoesNotExist:
             return None
         else:
-            if infant_dataset_obj:
-                cohort = Cohort(
-                    child_dob=child_dob,
-                    enrollment_date=enrollment_date,
-                    infant_hiv_exposed=infant_dataset_obj.infant_hiv_exposed,
-                    protocol=maternal_dataset_obj.protocol,
-                    mum_hiv_status=maternal_dataset_obj.mom_hivstatus,
-                    dtg=maternal_dataset_obj.preg_dtg,
-                    efv=maternal_dataset_obj.preg_efv,
-                    pi=maternal_dataset_obj.preg_pi).cohort_variable
-                return cohort
+            cohort = CohortAssignment(
+                child_dob=child_dob,
+                enrolment_dt=enrollment_date,
+                child_hiv_exposure=getattr(
+                    infant_dataset_obj, 'infant_hiv_exposed', None),
+                arv_regimen=getattr(
+                    maternal_dataset_obj, 'mom_pregarv_strat', None), )
+            return cohort.cohort_variable or None
