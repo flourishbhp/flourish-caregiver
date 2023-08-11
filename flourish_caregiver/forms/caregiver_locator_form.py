9ec6392ff4ecdf431e7c470ba41d6ffeac91b6b2
nmunatsibw <mcturnergarai@gmail.com>
2023-08-11 13:53:01 +0200

    :sparkles: feature(Admin): Refactor field validation and code cleaning in forms and admin directory.
    Implemented improved validation rules in caregiver_locator_form, allowing for certain fields to be optional to account for scenarios when not all the data is available. Modified caregiver_child_consent_form for better handling of field's state by introducing presence check before disabling them. Improved readability and added methods in subject_consent_admin to enhance manageability.

--- flourish_caregiver/forms/caregiver_locator_form.py
+++ flourish_caregiver/forms/caregiver_locator_form.py
@@ -1,9 +1,12 @@
+from itertools import chain
+
 from django import forms
+from django.db.models import ManyToManyField
 from edc_base.sites import SiteModelFormMixin
+from edc_constants.constants import YES
 from edc_form_validators import FormValidatorMixin
 
 from flourish_form_validations.form_validators import CaregiverLocatorFormValidator
-
 from ..models import CaregiverLocator
 from ..models import SubjectConsent
 
@@ -42,10 +45,52 @@ class CaregiverLocatorForm(SiteModelFormMixin, FormValidatorMixin, forms.ModelFo
         except SubjectConsent.DoesNotExist:
             pass
         else:
-            self.fields['first_name'].widget = forms.TextInput(attrs={'readonly': 'readonly'})
-            self.fields['last_name'].widget = forms.TextInput(attrs={'readonly': 'readonly'})
+            self.fields['first_name'].widget = forms.TextInput(
+                attrs={'readonly': 'readonly'})
+            self.fields['last_name'].widget = forms.TextInput(
+                attrs={'readonly': 'readonly'})
             self.initial['first_name'] = subject_consented.first_name
             self.initial['last_name'] = subject_consented.last_name
+
+    def compare_instance_fields(self, prev_instance=None):
+        exclude_fields = ['modified', 'created', 'user_created', 'user_modified',
+                          'hostname_created', 'hostname_modified', 'device_created',
+                          'device_modified', 'report_datetime', 'subject_identifier',
+                          'is_locator_updated', 'action_identifier',
+                          'tracking_identifier', 'related_tracking_identifier',
+                          'parent_tracking_identifier', 'study_maternal_identifier',
+                          'locator_date', 'may_sms'
+                          ]
+        if prev_instance:
+            other_values = self.model_to_dict(prev_instance, exclude=exclude_fields)
+            values = {key: self.data.get(key) or None for key in other_values.keys()}
+            return values != other_values
+        return False
+
+    def model_to_dict(self, instance, exclude):
+        opts = instance._meta
+        data = {}
+        for f in chain(opts.concrete_fields, opts.private_fields, opts.many_to_many):
+            if not getattr(f, 'editable', False):
+                continue
+            if exclude and f.name in exclude:
+                continue
+            if isinstance(f, ManyToManyField):
+                data[f.name] = [str(obj.id) for obj in f.value_from_object(instance)]
+                continue
+            data[f.name] = f.value_from_object(instance) or None
+        return data
+
+    def clean(self):
+        self.cleaned_data = super().clean()
+        subject_identifier = self.initial.get('subject_identifier', None)
+        prev_instance = CaregiverLocator.objects.get(
+            subject_identifier=subject_identifier)
+        has_changed = self.compare_instance_fields(prev_instance)
+        if not has_changed and self.cleaned_data.get('is_locator_updated') == YES:
+            raise forms.ValidationError(
+                'No changes detected. Please update at least one field.')
+        return self.cleaned_data
 
     class Meta:
         model = CaregiverLocator
