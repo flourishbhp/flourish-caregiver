26f37a9730214e1fe3cf65c0a683ea8e376e7636
Ame Diphoko <diphokoame@gmail.com>
2023-09-12 16:18:09 +0200

    remove import redefinition

--- flourish_caregiver/models/__init__.py
+++ flourish_caregiver/models/__init__.py
@@ -26,7 +26,7 @@ from .caregiver_phq_referral_fu import CaregiverPhqReferralFU
 from .caregiver_previously_enrolled import CaregiverPreviouslyEnrolled
 from .caregiver_requisition import CaregiverRequisition
 from .caregiver_social_work_referral import CaregiverSocialWorkReferral
-# from .cohort import Cohort
+from .cohort import Cohort
 from .covid_19 import Covid19
 from .enrollment import Enrollment
 from .flourish_consent_version import FlourishConsentVersion
@@ -94,108 +94,13 @@ from .tb_study_screening import TbStudyEligibility
 from .tb_visit_screening_women import TbVisitScreeningWomen
 from .ultrasound import UltraSound
 from .tb_adol_caregiver_consent import TbAdolChildConsent
-from .antenatal_enrollment import AntenatalEnrollment
-from .arvs_pre_pregnancy import ArvsPrePregnancy
-from .breastfeeding_questionnaire import BreastFeedingQuestionnaire
-from .caregiver_child_consent import CaregiverChildConsent
-from .caregiver_clinical_measurements import CaregiverClinicalMeasurements
-from .caregiver_clinical_measurements_fu import CaregiverClinicalMeasurementsFu
-from .caregiver_clinician_notes import ClinicianNotes, ClinicianNotesImage
-from .caregiver_contact import CaregiverContact
-from .caregiver_edinburgh_depr_screening import CaregiverEdinburghDeprScreening
-from .caregiver_edinburgh_post_referral import CaregiverEdinburghPostReferral
-from .caregiver_edinburgh_referral import CaregiverEdinburghReferral
-from .caregiver_edinburgh_referral_fu import CaregiverEdinburghReferralFU
-from .caregiver_gad_anxiety_screening import CaregiverGadAnxietyScreening
-from .caregiver_gad_post_referral import CaregiverGadPostReferral
-from .caregiver_gad_referral import CaregiverGadReferral
-from .caregiver_gad_referral_fu import CaregiverGadReferralFU
-from .caregiver_hamd_depr_screening import CaregiverHamdDeprScreening
-from .caregiver_hamd_post_referral import CaregiverHamdPostReferral
-from .caregiver_hamd_referral import CaregiverHamdReferral
-from .caregiver_hamd_referral_fu import CaregiverHamdReferralFU
-from .caregiver_locator import CaregiverLocator
-from .caregiver_phq_depr_screening import CaregiverPhqDeprScreening
-from .caregiver_phq_post_referral import CaregiverPhqPostReferral
-from .caregiver_phq_referral import CaregiverPhqReferral
-from .caregiver_phq_referral_fu import CaregiverPhqReferralFU
-from .caregiver_previously_enrolled import CaregiverPreviouslyEnrolled
-from .caregiver_requisition import CaregiverRequisition
 from .caregiver_requisition_result import CaregiverRequisitionResult
 from .caregiver_requisition_result import CaregiverResultValue
-from .caregiver_social_work_referral import CaregiverSocialWorkReferral
-from .covid_19 import Covid19
-from .cohort import Cohort
 from .cohort_schedules import CohortSchedules
-from .enrollment import Enrollment
-from .flourish_consent_version import FlourishConsentVersion
-from .food_security_questionnaire import FoodSecurityQuestionnaire
-from .hiv_disclosure_status import HIVDisclosureStatusA
-from .hiv_disclosure_status import HIVDisclosureStatusB
-from .hiv_disclosure_status import HIVDisclosureStatusC
-from .hiv_rapid_test_counseling import HIVRapidTestCounseling
-from .hiv_viralload_cd4 import HivViralLoadAndCd4
 from .interview_focus_group_interest import InterviewFocusGroupInterest
 from .interview_focus_group_interest_version_2 import InterviewFocusGroupInterestV2
 from .list_models import *
-from .locator_logs import LocatorLog, LocatorLogEntry
-from .maternal_arv import MaternalArvAtDelivery, MaternalArvTableAtDelivery
 from .maternal_arv_adherence import MaternalArvAdherence
-from .maternal_arv_during_preg import MaternalArvDuringPreg, MaternalArvTableDuringPreg
-from .maternal_arv_post_adherence import MaternalArvPostAdherence
-from .maternal_dataset import MaternalDataset
-from .maternal_delivery import MaternalDelivery
-from .maternal_diagnoses import MaternalDiagnoses
-from .maternal_hiv_interim_hx import MaternalHivInterimHx
-from .maternal_interim_idcc_data import MaternalInterimIdcc
 from .maternal_interim_idcc_data_version_2 import MaternalInterimIdccVersion2
-from .maternal_visit import MaternalVisit
-from .medical_history import MedicalHistory
-from .obsterical_history import ObstericalHistory
-from .offschedule import CaregiverOffSchedule
-from .onschedule import OnScheduleCohortAAntenatal, OnScheduleCohortBSec, \
-    OnScheduleCohortCSec
-from .onschedule import OnScheduleCohortABirth, OnScheduleCohortBFU, OnScheduleCohortCFU
-from .onschedule import OnScheduleCohortAEnrollment, OnScheduleCohortAFU
-from .onschedule import OnScheduleCohortAFUQuarterly, OnScheduleCohortCSecQuart
-from .onschedule import OnScheduleCohortAQuarterly, OnScheduleCohortBEnrollment
-from .onschedule import OnScheduleCohortASec, OnScheduleCohortCPool, \
-    OnScheduleCohortCQuarterly
-from .onschedule import OnScheduleCohortASecQuart, OnScheduleCohortBSecQuart
-from .onschedule import OnScheduleCohortATb2Months, OnScheduleCohortATb6Months
-from .onschedule import OnScheduleCohortBFUQuarterly, OnScheduleCohortCFUQuarterly
-from .onschedule import OnScheduleCohortBQuarterly, OnScheduleCohortCEnrollment
-from .relationship_father_involvement import RelationshipFatherInvolvement
-from .screening_preg_women import ScreeningPregWomen
-from .screening_prior_bhp_participants import ScreeningPriorBhpParticipants
-from .signals import antenatal_enrollment_on_post_save
-from .signals import caregiver_child_consent_on_post_save
-from .signals import maternal_dataset_on_post_save
-from .signals import subject_consent_on_post_save
 from .socio_demographic_data import HouseHoldDetails
-from .socio_demographic_data import SocioDemographicData
-from .subject_consent import SubjectConsent
-from .substance_use_during_preg import SubstanceUseDuringPregnancy
-from .substance_use_prior_preg import SubstanceUsePriorPregnancy
-from .tb_adol_caregiver_consent import TbAdolChildConsent
-from .tb_adol_caregiver_consent import TbAdolConsent
-from .tb_adol_screening import TbAdolEligibility
-from .tb_engagement import TbEngagement
-from .tb_history_preg import TbHistoryPreg
-from .tb_informed_consent import TbInformedConsent
-from .tb_int_transcription import TbInterviewTranscription
-from .tb_int_translation import TbInterviewTranslation
-from .tb_interview import TbInterview
-from .tb_knowledge import TbKnowledge
-from .tb_off_study import TbOffStudy
-from .tb_presence_household_members import TbPresenceHouseholdMembers
-from .tb_referral import TbReferral
-from .tb_referral_outcomes import TbReferralOutcomes
-from .tb_routine_health_screen import TbRoutineHealthScreen
-from .tb_routine_health_screen_v2 import TbRoutineHealthEncounters, \
-    TbRoutineHealthScreenV2
-from .tb_screen_preg import TbScreenPreg
-from .tb_study_screening import TbStudyEligibility
-from .tb_visit_screening_women import TbVisitScreeningWomen
-from .ultrasound import UltraSound
 from .post_hiv_rapid_testing_and_conseling import PostHivRapidTestAndConseling
