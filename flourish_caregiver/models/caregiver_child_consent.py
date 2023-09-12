bfb383e309467b1f64233ef855672318678039a1
Ame Diphoko <diphokoame@gmail.com>
2023-09-12 16:56:10 +0200

    minor fix

--- flourish_caregiver/models/caregiver_child_consent.py
+++ flourish_caregiver/models/caregiver_child_consent.py
@@ -162,9 +162,6 @@ class CaregiverChildConsent(SiteModelMixin, NonUniqueSubjectIdentifierFieldMixin
         editable=False)
 
     def save(self, *args, **kwargs):
-        if self.subject_identifier and not self.cohort:
-            self.cohort = self.assign_enrol_instance_cohort
-
         self.preg_enroll = self.is_preg
 
         eligibility_criteria = CaregiverChildConsentEligibility(
