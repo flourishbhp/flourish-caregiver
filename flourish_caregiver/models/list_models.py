7bc7f33e89a5927a2b127617fd6d655978f1ffef
Moses Chawawa <moseschawawa@gmail.com>
2023-12-08 12:16:48 +0200

    Medical history manytomany

--- flourish_caregiver/models/list_models.py
+++ flourish_caregiver/models/list_models.py
@@ -91,3 +91,7 @@ class ExpenseContributors(ListModelMixin, BaseUuidModel):
 
 class TBTests(ListModelMixin, BaseUuidModel):
     pass
+
+
+class GeneralSymptoms(ListModelMixin, BaseUuidModel):
+    pass
