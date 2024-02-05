8d7acae9e372fcc96f3b703c3c0bf6857ae69865
nmunatsibw <mcturnergarai@gmail.com>
2024-02-05 14:35:08 +0200

    :ambulance: fix(Form-validations): Add HITSScreeningForm and related model changes
    Implemented a new form validator, HITSScreeningFormValidator. The HITSScreeningForm model was updated to include a new field 'in_relationship'. Also, some fields were made optional. Finally, the validator was integrated with HITSScreening form.

--- flourish_caregiver/models/hits_screening.py
+++ flourish_caregiver/models/hits_screening.py
@@ -1,29 +1,43 @@
 from django.db import models
+from edc_constants.choices import YES_NO
 
 from flourish_caregiver.choices import PARTNER_ACTIONS_CHOICES
 from flourish_caregiver.models.model_mixins import CrfModelMixin
 
 
 class HITSScreening(CrfModelMixin):
+    in_relationship = models.CharField(
+        verbose_name='Are you currently in a relationship?',
+        choices=YES_NO,
+        max_length=3)
+
     physical_hurt = models.CharField(
         verbose_name='How often does your partner physically hurt you?',
         choices=PARTNER_ACTIONS_CHOICES,
-        max_length=5)
+        max_length=5,
+        blank=True,
+        null=True)
 
     insults = models.CharField(
         verbose_name='How often does your partner insult or talk down to you?',
         choices=PARTNER_ACTIONS_CHOICES,
-        max_length=5)
+        max_length=5,
+        blank=True,
+        null=True)
 
     threaten = models.CharField(
         verbose_name='How often does your partner threaten you with harm?',
         choices=PARTNER_ACTIONS_CHOICES,
-        max_length=5)
+        max_length=5,
+        blank=True,
+        null=True)
 
     screem_curse = models.CharField(
         verbose_name='How often does your partner scream or curse at you?',
         choices=PARTNER_ACTIONS_CHOICES,
-        max_length=5)
+        max_length=5,
+        blank=True,
+        null=True)
 
     score = models.IntegerField(
         verbose_name='Total HITS Score',
