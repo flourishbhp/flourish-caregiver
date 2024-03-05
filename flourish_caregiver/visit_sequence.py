a35441f1a4449c59a37d29b066cee6cd04b14dfa
nmunatsibw <mcturnergarai@gmail.com>
2024-03-05 19:29:41 +0200

    Refactor visit sequence logic in Flourish Caregiver
    This refactoring simplifies the initialization of appointment-related properties and extends the logic to handle visit_code_sequence. It also includes a sequence_query to prioritize the previous appointment by visit_code_sequence. Overall, these changes streamline visit tracking and improve sequence maintenance.

--- flourish_caregiver/visit_sequence.py
+++ flourish_caregiver/visit_sequence.py
@@ -1,4 +1,5 @@
 from django.apps import apps as django_apps
+from django.db.models import Q
 from edc_visit_tracking.visit_sequence import VisitSequence, VisitSequenceError
 
 from .helper_classes.utils import get_previous_by_appt_datetime
@@ -6,28 +7,29 @@ from .helper_classes.utils import get_previous_by_appt_datetime
 
 class VisitSequence(VisitSequence):
     def __init__(self, appointment=None):
-        self.initialize_appointment_related_properties(appointment)
-
-    def initialize_appointment_related_properties(self, appointment):
         self.appointment = appointment
-        self.appointment_model_cls = appointment.__class__
+        self.appointment_model_cls = self.appointment.__class__
         self.model_cls = getattr(
             self.appointment_model_cls,
             self.appointment_model_cls.related_visit_model_attr()
         ).related.related_model
-        self.subject_identifier = appointment.subject_identifier
-        self.visit_schedule_name = appointment.visit_schedule_name
-        self.visit_code = appointment.visit_code
-
-        prev_visit = appointment.schedule.visits.previous(self.visit_code)
+        self.subject_identifier = self.appointment.subject_identifier
+        self.visit_schedule_name = self.appointment.visit_schedule_name
+        self.visit_code = self.appointment.visit_code
+        previous_visit = self.appointment.schedule.visits.previous(
+            self.visit_code)
         self.previous_appointment = get_previous_by_appt_datetime(self.appointment)
 
         try:
             self.previous_visit_code = getattr(
-                self.previous_appointment, 'visit_code', None) or prev_visit.code
+                self.previous_appointment, 'visit_code', None) or previous_visit.code
         except AttributeError:
             self.previous_visit_code = None
-
+        self.sequence_query = Q()
+        if self.visit_code == self.previous_visit_code:
+            previous_visit_code_sequence = getattr(
+                self.previous_appointment, 'visit_code_sequence', 0)
+            self.sequence_query = Q(visit_code_sequence=previous_visit_code_sequence)
         self.previous_visit_missing = self.previous_visit_code and not self.previous_visit
 
     @property
@@ -91,8 +93,10 @@ class VisitSequence(VisitSequence):
 
     def get_previous_appointment(self):
         return self.appointment_model_cls.objects.filter(
+            self.sequence_query,
             subject_identifier=self.subject_identifier,
-            visit_code=self.previous_visit_code).order_by('-visit_code_sequence').first()
+            visit_code=self.previous_visit_code).order_by(
+            '-visit_code_sequence').first()
 
     def get_visit_for_appointment(self, appointment):
         try:
