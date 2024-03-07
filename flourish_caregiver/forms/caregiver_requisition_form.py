e0f4a253415f426141937e1ead48b71d044cd294
Moses Chawawa <moseschawawa@gmail.com>
2024-03-07 11:54:17 +0200

    Requisition validations

--- flourish_caregiver/forms/caregiver_requisition_form.py
+++ flourish_caregiver/forms/caregiver_requisition_form.py
@@ -38,6 +38,50 @@ class CaregiverRequisitionForm(SubjectModelFormMixin, RequisitionFormMixin,
                     'requisition_datetime':
                     f'Invalid. Cannot be before date of visit {formatted}.'})
 
+        from arrow.arrow import Arrow
+
+
+class CaregiverRequisitionForm(SubjectModelFormMixin, RequisitionFormMixin,
+                               SenaiteRequisitionFormValidatorMixin,
+                               FormValidatorMixin):
+
+    requisition_identifier = forms.CharField(
+        label='Requisition identifier',
+        widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+
+    def clean(self):
+        self.visit_obj = self.cleaned_data.get('maternal_visit')
+        self.subject_identifier = self.cleaned_data.get(
+            'maternal_visit').subject_identifier
+        self.validate_estimated_volume()
+        super().clean()
+
+    def validate_requisition_datetime(self):
+        requisition_datetime = self.cleaned_data.get('requisition_datetime')
+        maternal_visit = self.cleaned_data.get('maternal_visit')
+        if requisition_datetime:
+            requisition_datetime = Arrow.fromdatetime(
+                requisition_datetime, requisition_datetime.tzinfo).to('utc').datetime
+            if requisition_datetime < maternal_visit.report_datetime:
+                formatted = timezone.localtime(maternal_visit.report_datetime).strftime(
+                    convert_php_dateformat(settings.SHORT_DATETIME_FORMAT))
+                raise forms.ValidationError({
+                    'requisition_datetime':
+                    f'Invalid. Cannot be before date of visit {formatted}.'})
+
+    def validate_estimated_volume(self):
+        # required fields
+        panel = self.cleaned_data.get('panel')
+        estimated_volume = self.cleaned_data.get('estimated_volume')
+
+        if panel.name == 'breast_milk':
+            if 0 > estimated_volume or estimated_volume > 20:
+                raise forms.ValidationError({'estimated_volume': 'The estimated volume should be between 0 & 20 ml'})
+
+    class Meta:
+        model = CaregiverRequisition
+        fields = '__all__'
+
     class Meta:
         model = CaregiverRequisition
         fields = '__all__'
