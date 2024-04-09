a1e8989b1f186783df039cda8bbac29998ab1f21
Moses Chawawa <moseschawawa@gmail.com>
2024-04-09 10:44:01 +0200

    add/pars form

--- flourish_caregiver/forms/__init__.py
+++ flourish_caregiver/forms/__init__.py
@@ -93,3 +93,4 @@ from .tb_study_screening_form import TbStudyScreeningForm
 from .tb_visit_screening_women_form import TbVisitScreeningWomenForm
 from .ultrasound_form import UltraSoundForm
 from .caregiver_safi_stigma_form import CaregiverSafiStigmaForm
+from .parent_adol_relationship_scale_forms import ParentAdolRelationshipScaleForm
