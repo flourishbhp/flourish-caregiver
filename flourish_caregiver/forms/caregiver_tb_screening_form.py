ec9dfbe62d7d49b4ee49bb81b9dd12b1a2055e03
nmunatsibw <mcturnergarai@gmail.com>
2024-06-14 12:10:34 +0200

    (refactor): Add PreviousResults support to CaregiverTBScreeningAdmin and form
    This update incorporates the PreviousResultsAdminMixin into the CaregiverTBScreeningAdmin class to facilitate viewing of previous test results. Simultaneously, the CaregiverTBScreeningForm was also updated to include the PreviousResultsFormMixin, enhancing form's extensibility and organization.
    
    Signed-off-by: nmunatsibw <mcturnergarai@gmail.com>

--- flourish_caregiver/forms/caregiver_tb_screening_form.py
+++ flourish_caregiver/forms/caregiver_tb_screening_form.py
@@ -2,11 +2,13 @@ from django import forms
 
 from flourish_caregiver.forms.form_mixins import SubjectModelFormMixin
 from flourish_caregiver.models.caregiver_tb_screening import CaregiverTBScreening
+from flourish_child.forms import PreviousResultsFormMixin
 from flourish_form_validations.form_validators.caregiver_tb_screening_form_validator import \
     CaregiverTBScreeningFormValidator
 
 
-class CaregiverTBScreeningForm(SubjectModelFormMixin, forms.ModelForm):
+class CaregiverTBScreeningForm(SubjectModelFormMixin, PreviousResultsFormMixin,
+                               forms.ModelForm):
     form_validator_cls = CaregiverTBScreeningFormValidator
 
     class Meta:
