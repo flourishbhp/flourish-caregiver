8e42d837ac94fc27eac6dd9c3a1f6b321eb99909
nmunatsibw <mcturnergarai@gmail.com>
2024-07-04 09:11:47 +0200

    (refactor): Simplify caregiver TB screening and diagnosis functionalities
    Changes in this commit include the simplification of the evaluation for TB in the TBDiagnosis class. This includes the structuring of the evaluate_for_tb method and the creation of the evaluate_screening_conditions method which serves to tidy up the logic of evaluating whether the subject meets the conditions for TB screening.
    
    Additionally, the CaregiverTBScreeningForm was amended to include a method named get_keys_before which retrieves all dictionary keys before a specified key. This change improves the structure and readability of the form class. Import statements from various other classes were also adjusted.
    
    Signed-off-by: nmunatsibw <mcturnergarai@gmail.com>

--- flourish_caregiver/forms/caregiver_tb_screening_form.py
+++ flourish_caregiver/forms/caregiver_tb_screening_form.py
@@ -1,5 +1,3 @@
-from django import forms
-
 from flourish_caregiver.forms.form_mixins import SubjectModelFormMixin
 from flourish_caregiver.models.caregiver_tb_screening import CaregiverTBScreening
 from flourish_child.forms import PreviousResultsFormMixin
@@ -7,9 +5,17 @@ from flourish_form_validations.form_validators.caregiver_tb_screening_form_valid
     CaregiverTBScreeningFormValidator
 
 
-class CaregiverTBScreeningForm(SubjectModelFormMixin, PreviousResultsFormMixin,
-                               forms.ModelForm):
+class CaregiverTBScreeningForm(PreviousResultsFormMixin, SubjectModelFormMixin):
     form_validator_cls = CaregiverTBScreeningFormValidator
+
+    def get_keys_before(self, dict):
+        key_stop = "maternal_visit"
+        return_list = []
+        for key in dict:
+            if key == key_stop:
+                break
+            return_list.append(key)
+        return return_list
 
     class Meta:
         model = CaregiverTBScreening
