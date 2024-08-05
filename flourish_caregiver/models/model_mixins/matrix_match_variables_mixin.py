41c4f95a2aeb7983a3e1494e86b7133c3f6350b4
Ame Diphoko <diphokoame@gmail.com>
2024-08-05 15:07:55 +0200

    update: cohort variable for age and bmi at date

--- flourish_caregiver/models/model_mixins/matrix_match_variables_mixin.py
+++ flourish_caregiver/models/model_mixins/matrix_match_variables_mixin.py
@@ -31,3 +31,22 @@ class MatrixMatchVariablesMixin:
     @property
     def child_gender(self):
         return getattr(self.caregiver_child_consent, 'gender', None)
+
+    def child_age_at_date(self, reference_date=get_utcnow().date()):
+        dob = getattr(self.caregiver_child_consent, 'child_dob', None)
+        if not dob:
+            return None
+        _age = age(dob, reference_date)
+        _age = _age.years + _age.months / 12 + _age.days / 365
+        return _age
+
+    def child_bmi_at_date(self, reference_date=get_utcnow().date()):
+        try:
+            model_obj = self.child_clinical_measurements_cls.objects.filter(
+                child_visit__subject_identifier=self.subject_identifier,
+                report_datetime__date__lt=reference_date).latest(
+                    'report_datetime')
+        except self.child_clinical_measurements_cls.DoesNotExist:
+            return None
+        else:
+            return model_obj.child_weight_kg / pow(model_obj.child_height/100, 2)
